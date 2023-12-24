46b45036d9ce79f5a07ddae396eebf324f588d1f
AliHakim773 <AliHakim773@gmail.com>
2023-12-24 22:44:01 +0200

    added auth routes

--- tulp-backend/index.js
+++ tulp-backend/index.js
@@ -1,8 +1,15 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const connectToMongoDB = require("./configs/db.config")
 
 const app = express()
+app.use(express.json())
+app.use(cors())
+
+//auth routes
+const authRoutes = require("./routes/auth.routes")
+app.use("/auth", authRoutes)
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
