ab10e43c9ea6c2a976e458dc0be6a3e4a73eec4d
AliHakim773 <AliHakim773@gmail.com>
2023-12-26 18:32:04 +0200

    added assignment schema

---
+++ tulp-backend/models/assignment.model.js
@@ -0,0 +1,28 @@
+const { default: mongoose } = require("mongoose")
+const SubmissionSchema = require("./submission.model")
+
+const AssignmentSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: "Assignments must have a title",
+    minlength: [3, "Title length must be more than 3"],
+    maxlength: [40, "Title length must be less than 40"],
+  },
+  content: {
+    type: String,
+    maxlength: [400, "Can't be more than 400 chars"],
+    requiered: "Feed content cant be empty",
+  },
+  sender: {
+    type: mongoose.Types.ObjectId,
+    requiered: "Sernder ID is requiered",
+  },
+  files: [
+    {
+      type: String,
+    },
+  ],
+  submissions: [SubmissionSchema],
+})
+
+module.exports = AssignmentSchema
