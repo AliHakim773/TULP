c5226ec1da8eb57852a7fad50e787d5d33b0f097
AliHakim773 <AliHakim773@gmail.com>
2023-12-26 13:37:04 +0200

    added login function

--- tulp-backend/controllers/auth.controllers.js
+++ tulp-backend/controllers/auth.controllers.js
@@ -2,6 +2,39 @@ const User = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 
+const login = async (rep, res) => {
+  const { username, password } = rep.body
+
+  // validation
+  if (!username) return res.status(400).send({ error: "Username is required" })
+
+  if (!password) return res.status(400).send({ error: "Password is required" })
+
+  try {
+    const user = await User.findOne({ username })
+    if (!user)
+      return res.status(400).send({ message: "Wrong username or password" })
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+    if (!isValidPassword)
+      res.status(400).send({ message: "Wrong username or password" })
+
+    const { password: hashedPassword, _id, ...userDetails } = user.toJSON()
+
+    const token = jwt.sign(
+      {
+        ...userDetails,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30 days" }
+    )
+
+    res.status(200).send({ user: userDetails, token })
+  } catch (e) {
+    res.status(500).send({ error: e.message })
+  }
+}
+
 const register = async (rep, res) => {
   const { username, password, firstName, lastName, email, role } = rep.body
 
@@ -34,25 +67,24 @@ const register = async (rep, res) => {
       email,
       role: role || "student",
     })
+
+    const { password: hashedPassword, _id, ...userDetails } = user.toJSON()
+
     const token = jwt.sign(
       {
-        username,
-        password,
-        firstName,
-        lastName,
-        email,
-        role: role || "student",
+        ...userDetails,
       },
       process.env.JWT_SECRET,
       { expiresIn: "30 days" }
     )
 
-    res.status(200).send({ user, token })
+    res.status(200).send({ user: userDetails, token })
   } catch (e) {
     res.status(500).send({ error: e.message })
   }
 }
 
 module.exports = {
+  login,
   register,
 }
