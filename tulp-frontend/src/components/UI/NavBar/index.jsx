0c3f4f4149ad0a3c8746036042fd05772420d43c
AliHakim773 <AliHakim773@gmail.com>
2023-12-26 13:14:40 +0200

    added basic logged out navbar

--- tulp-frontend/src/components/UI/NavBar/index.jsx
+++ tulp-frontend/src/components/UI/NavBar/index.jsx
@@ -1,8 +1,36 @@
 import React from "react"
+import Logo from "../../../assets/images/logo-no-bg.png"
+import { Link } from "react-router-dom"
 import "./styles.css"
 
 const NavBar = () => {
-  return <div>NavBar</div>
+  return (
+    <nav className='home-nav flex center blue-border-bottom'>
+      <div className='home-nav-wrapper flex'>
+        <div className='home-logo h-100'>
+          <Link to={"/"} className='h-100'>
+            <img src={Logo} alt='TULP Logo' />
+          </Link>
+        </div>
+        <ul className='nav-links flex h-100'>
+          <li className='h-100'>
+            <Link
+              to={"/register"}
+              className='nav-link h-100 flex center blue-3-txt semi-bold'>
+              Register
+            </Link>
+          </li>
+          <li className='h-100'>
+            <Link
+              to={"/login"}
+              className='nav-link h-100 flex center blue-3-txt semi-bold'>
+              Login
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
