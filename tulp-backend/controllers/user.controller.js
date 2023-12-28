8f24e8c988640918e9163f1c8e8b3fa9dea2dc1b
AliHakim773 <AliHakim773@gmail.com>
2023-12-28 11:43:44 +0200

    updated image unlink logic

--- tulp-backend/controllers/user.controller.js
+++ tulp-backend/controllers/user.controller.js
@@ -7,38 +7,50 @@ const editUser = async (req, res) => {
   const { username, password, email, firstName, lastName, birth, aboutMe } =
     req.body
 
-  const image = req.file
-  const imageUrl = image ? image.path : ""
-
-  const user = await User.findOne({ _id: id })
-  const oldImageUrl = user.imageUrl
-
-  user.username = username || user.username
-  user.password = password || user.password
-  user.email = email || user.email
-  user.firstName = firstName || user.firstName
-  user.lastName = lastName || user.lastName
-  user.birth = birth || user.birth
-  user.aboutMe = aboutMe || user.aboutMe
-  user.imageUrl = imageUrl || user.imageUrl
-
-  await user.save()
-
-  if (oldImageUrl && oldImageUrl !== "uploads/defualt.png") {
-    await fs.unlink(oldImageUrl)
-  }
-
-  const { password: hashedPassword, ...userDetails } = user.toJSON()
-  const token = jwt.sign(
-    {
-      ...userDetails,
-    },
-    process.env.JWT_SECRET,
-    { expiresIn: "30 days" }
-  )
-
-  res.status(200).send({ user: userDetails, token })
   try {
+    const user = await User.findOne({ _id: id })
+
+    let oldImageUrl = user.imageUrl
+    let imageUrl = user.imageUrl
+    let removeImage = false
+
+    if (req.file) {
+      imageUrl = req.file.path
+      removeImage = true
+    } else if (req.body.removeImage) {
+      imageUrl = "uploads/default.png"
+      removeImage = true
+    }
+
+    if (removeImage) {
+      if (oldImageUrl !== "uploads/default.png") {
+        fs.unlink(oldImageUrl, (err) => {
+          if (err) res.status(404).send({ error: e })
+        })
+      }
+    }
+
+    user.username = username || user.username
+    user.password = password || user.password
+    user.email = email || user.email
+    user.firstName = firstName || user.firstName
+    user.lastName = lastName || user.lastName
+    user.birth = birth || user.birth
+    user.aboutMe = aboutMe || user.aboutMe
+    user.imageUrl = imageUrl || user.imageUrl
+
+    await user.save()
+
+    const { password: hashedPassword, ...userDetails } = user.toJSON()
+    const token = jwt.sign(
+      {
+        ...userDetails,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30 days" }
+    )
+
+    res.status(200).send({ user: userDetails, token })
   } catch (e) {
     res.status(500).send({ error: e.message })
   }
