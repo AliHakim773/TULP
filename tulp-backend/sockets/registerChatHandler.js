cf4f21d97cf9595443dbe190d660fb06a1c354ec
AliHakim773 <AliHakim773@gmail.com>
2023-12-28 17:50:43 +0200

    added leave room socket

--- tulp-backend/sockets/registerChatHandler.js
+++ tulp-backend/sockets/registerChatHandler.js
@@ -5,7 +5,14 @@ const registerChatHandler = (io, socket) => {
     socket.emit("chat:join-room", room)
   }
 
+  const leaveRoom = (room, cb) => {
+    socket.leave(room)
+    cb(`Left ${room}`)
+    socket.emit("chat:leave-room", room)
+  }
+
   socket.on("chat:join-room", joinRoom)
+  socket.on("chat:leave-room", leaveRoom)
 }
 
 module.exports = registerChatHandler
