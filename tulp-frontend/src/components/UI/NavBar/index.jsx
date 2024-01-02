362f60effc63c0adbe276e805e729e4d792fa7fd
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 17:54:18 +0200

    basic nav change on user login

--- tulp-frontend/src/components/UI/NavBar/index.jsx
+++ tulp-frontend/src/components/UI/NavBar/index.jsx
@@ -1,9 +1,14 @@
 import React from "react"
 import Logo from "../../../assets/images/logo.png"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import "./styles.css"
+import useNavBarLogic from "./useNavBarLogic"
+import { clearUser } from "../../../core/redux/userSlice"
 
 const NavBar = () => {
+  const navigate = useNavigate()
+  const { user, isLoggedIn, dispatch } = useNavBarLogic()
+
   return (
     <nav className='home-nav flex center border-bottom'>
       <div className='home-nav-wrapper flex'>
@@ -13,20 +18,38 @@ const NavBar = () => {
           </Link>
         </div>
         <ul className='nav-links flex h-100'>
-          <li className='h-100'>
-            <Link
-              to={"/register"}
-              className='nav-link h-100 flex center blue-3-txt semi-bold'>
-              Register
-            </Link>
-          </li>
-          <li className='h-100'>
-            <Link
-              to={"/login"}
-              className='nav-link h-100 flex center blue-3-txt semi-bold'>
-              Login
-            </Link>
-          </li>
+          {!isLoggedIn ? (
+            <>
+              <li className='h-100'>
+                <Link
+                  to={"/register"}
+                  className='nav-link h-100 flex center blue-3-txt semi-bold'>
+                  Register
+                </Link>
+              </li>
+              <li className='h-100'>
+                <Link
+                  to={"/login"}
+                  className='nav-link h-100 flex center blue-3-txt semi-bold'>
+                  Login
+                </Link>
+              </li>
+            </>
+          ) : (
+            <>
+              <li className='h-100'>
+                <p
+                  onClick={() => {
+                    localStorage.removeItem("token")
+                    dispatch(clearUser())
+                    navigate("/")
+                  }}
+                  className='nav-link h-100 flex center blue-3-txt semi-bold'>
+                  Logout
+                </p>
+              </li>
+            </>
+          )}
         </ul>
       </div>
     </nav>
