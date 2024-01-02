e9294656003ab4052abfb989ee2550d8ca33741e
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 13:22:05 +0200

    added onchange and values to inout comp

--- tulp-frontend/src/components/Base/InputField/index.jsx
+++ tulp-frontend/src/components/Base/InputField/index.jsx
@@ -6,13 +6,22 @@ const InputField = ({
   id = text,
   type = "text",
   placeholder = "placeholder",
+  value,
+  handleOnChange,
 }) => {
   return (
     <div className='input-field flex column w-100'>
       <label className='w-100' htmlFor={id}>
         {text}
       </label>
-      <input className='w-100' type={type} id={id} placeholder={placeholder} />
+      <input
+        className='w-100'
+        value={value}
+        onChange={handleOnChange}
+        type={type}
+        id={id}
+        placeholder={placeholder}
+      />
     </div>
   )
 }
