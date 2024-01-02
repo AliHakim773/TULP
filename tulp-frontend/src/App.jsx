0de020df0f84f52c80b9aaf7e0b1179a35c371c3
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 16:00:53 +0200

    added store provider

--- tulp-frontend/src/App.jsx
+++ tulp-frontend/src/App.jsx
@@ -1,19 +1,23 @@
+import { Provider } from "react-redux"
 import LandingPage from "./pages/LandingPage"
 import PageNotFound from "./pages/PageNotFound"
 import RegisterPage from "./pages/RegisterPage"
 import "./styles/index.css"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { store } from "./core/redux/store"
 
 const App = () => {
   return (
     <div className='app'>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<LandingPage />} />
-          <Route path='/register' element={<RegisterPage />} />
-          <Route path='*' element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<LandingPage />} />
+            <Route path='/register' element={<RegisterPage />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </div>
   )
 }
