ded6ebfab3f1eac1068290ba14c3edf9ad8d39aa
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 15:34:46 +0200

    added axios config

---
+++ tulp-frontend/src/core/axios/index.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+import { local } from "../helpers/localstorage"
+
+axios.defaults.baseURL = "http://localhost:8000/"
+
+export const sendRequest = async ({ route, method = "GET", body }) => {
+  const token = local("token")
+
+  const authorizationHeader = token
+
+  console.log(authorizationHeader)
+
+  const response = await axios.request({
+    url: route,
+    method,
+    data: body,
+    headers: {
+      Authorization: authorizationHeader,
+      "Content-Type": "application/json",
+    },
+  })
+
+  return response.data
+}
