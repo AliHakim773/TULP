2f20237fa2fd1bd2d8ba2488797d02f9ae70b7b9
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 19:26:02 +0200

    get info from redux

--- tulp-frontend/src/components/ProfileCard/index.jsx
+++ tulp-frontend/src/components/ProfileCard/index.jsx
@@ -1,26 +1,32 @@
 import React from "react"
 import "./styles.css"
+import { useSelector } from "react-redux"
+import { extractUserSlice } from "../../core/redux/userSlice"
+import { Link } from "react-router-dom"
 
 const ProfileCard = () => {
+  const userSlice = useSelector(extractUserSlice)
+
   return (
     <div className='profile-card'>
       <div className='profile-card-1 flex column center w-100'>
-        <div className='profile-role'>student</div>
+        <div className='profile-role'>{userSlice.role}</div>
         <div className='profile-img'>
-          <img
-            src={`http://localhost:8000/uploads/image-1703937457200.png`}
-            alt=''
-          />
+          <img src={`http://localhost:8000/${userSlice.imageUrl}`} alt='' />
         </div>
-        <div className='profile-username'>John Doe</div>
+        <div className='profile-username'>{userSlice.username}</div>
       </div>
       <div className='profile-line' />
       <div className='profile-card-2 flex column center w-100'>
         {window.location.pathname !== "/edit-profile" && (
-          <button className='profile-card-2-btn semi-bold'>Edit Profile</button>
+          <Link to={"/edit-profile"} className='profile-card-2-btn semi-bold'>
+            Edit Profile
+          </Link>
         )}
         <div className='profile-info flex column'>
-          <div className='profile-item'>Full Name: John Hassan Doe</div>
+          <div className='profile-item'>
+            Full Name: {userSlice.firstName} {userSlice.lastName}
+          </div>
           <div className='profile-item'>Graduated At: John Hassan Doe</div>
           <div className='profile-item'>Degree: John Hassan Doe</div>
           <div className='profile-item'>Birth Date: 2000/1/1</div>
