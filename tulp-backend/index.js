ef9d7ea5a57ce5fc04832795cf664baaede4aea3
AliHakim773 <AliHakim773@gmail.com>
2024-01-02 11:50:19 +0200

    configured admin ui dependancy

--- tulp-backend/index.js
+++ tulp-backend/index.js
@@ -6,8 +6,10 @@ const connectToMongoDB = require("./configs/db.config")
 const siteRoutes = require("./routes/index.routes")
 const onConnection = require("./sockets")
 const { Server } = require("socket.io")
+const { instrument } = require("@socket.io/admin-ui")
 
 const app = express()
+app.use(express.static("./node_modules/@socket.io/admin-ui/ui/dist"))
 
 app.use(cors())
 app.use(express.json())
@@ -16,11 +18,18 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")))
 siteRoutes(app)
 
 const port = process.env.PORT || 5000
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log("Server listining on PORT: ", port)
 
   connectToMongoDB()
 })
 
-const io = new Server(app)
+const io = new Server(server)
 io.on("connection", onConnection)
+
+instrument(io, {
+  auth: false,
+  mode: "development",
+})
+
+module.exports = { io }
