c02c5d00fb9eaf09615e95b6c8191cb52dcb4e0b
AliHakim773 <AliHakim773@gmail.com>
2024-01-03 10:44:33 +0200

    changed responsive styles

--- tulp-frontend/src/pages/EditProfile/index.jsx
+++ tulp-frontend/src/pages/EditProfile/index.jsx
@@ -2,14 +2,20 @@ import React from "react"
 import "./styles.css"
 import BasicLayout from "../../components/UI/BasicLayout"
 import ProfileCard from "../../components/ProfileCard"
+import EditProfileForm from "../../components/EditProfileForm"
 
 const EditProfile = () => {
   return (
     <BasicLayout>
-      <div className='main-edit section-content flex'>
-        <aside className='edit-aside'>
-          <ProfileCard />
-        </aside>
+      <div className='edit-section w-100 flex'>
+        <div className='main-edit w-100 flex'>
+          <aside className='edit-aside'>
+            <ProfileCard />
+          </aside>
+          <section className='edit-form-section'>
+            <EditProfileForm />
+          </section>
+        </div>
       </div>
     </BasicLayout>
   )
