2e05a8948ce5181e752c71e6b95455cb10c09307
AliHakim773 <AliHakim773@gmail.com>
2024-01-06 18:45:15 +0200

    Backend: fixed import

--- tulp-backend/routes/assignment.routes.js
+++ tulp-backend/routes/assignment.routes.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { addAssignment } = require("../controllers/assignment.controller")
+const { addAssignment } = require("../controllers/assignment.controllers")
 const router = express.Router()
 
 router.post("/:classId", addAssignment)
