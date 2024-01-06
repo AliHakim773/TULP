b13d441acca225c10fb3688da9fab89a27b4b503
AliHakim773 <AliHakim773@gmail.com>
2024-01-06 15:50:35 +0200

    Frontend: added Postfield comp to landing feed

--- tulp-frontend/src/components/LandingFeed/index.jsx
+++ tulp-frontend/src/components/LandingFeed/index.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import "./styles.css"
+import PostField from "./PostField"
 
 const LandingFeed = () => {
-  return <main className='landing-feed-main'>Main</main>
+  return (
+    <main className='landing-feed-main border'>
+      <PostField />
+    </main>
+  )
 }
 
 export default LandingFeed
