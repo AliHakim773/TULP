a7ea04beb925e8cf4eaf2a434003bc42ca9ad010
AliHakim773 <AliHakim773@gmail.com>
2024-01-06 16:35:10 +0200

    Frontend: added post comp basic design

---
+++ tulp-frontend/src/components/LandingFeed/Post/index.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import "./styles.css"
+import comment from "../../../assets/svgs/comment.svg"
+import like from "../../../assets/svgs/like.svg"
+import liked from "../../../assets/svgs/liked.svg"
+
+const Post = ({ post }) => {
+  const { username, created_at, content, likes, comments, image_url } = post
+  return (
+    <div className='post w-100 shadow flex column'>
+      <div className='post-header flex'>
+        <div className='post-id flex'>
+          <div className='blue-3-txt semi-bold'>{username}</div>
+          <div className='post-time grey-1-txt'>- {created_at} ago</div>
+        </div>
+        <div className='post-img'>
+          <img src={image_url} alt='user Image' />
+        </div>
+      </div>
+      <div className='post-body'>{content}</div>
+      <div className='post-footer flex'>
+        <div className='likes flex'>
+          {likes}{" "}
+          <div className='post-icon'>
+            <img src={like} alt='likes' />
+          </div>
+        </div>
+        <div className='comments flex'>
+          {comments.length}{" "}
+          <div className='post-icon'>
+            <img src={comment} alt='comment' />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
