8abbe19ed9325eb1a23fb38b537ff566c597aab0
AliHakim773 <AliHakim773@gmail.com>
2024-01-06 16:34:33 +0200

    Frontend: added post comp with test posts

--- tulp-frontend/src/components/LandingFeed/PostFeed/index.jsx
+++ tulp-frontend/src/components/LandingFeed/PostFeed/index.jsx
@@ -1,8 +1,41 @@
 import React from "react"
 import "./styles.css"
+import Post from "../Post"
 
 const PostFeed = () => {
-  return <div></div>
+  return (
+    <div className='post-feed'>
+      <Post post={test_post} />
+    </div>
+  )
 }
-
+const test_post = {
+  _id: "asdad",
+  username: "ali",
+  image_url: "uploads/defualt.png",
+  created_at: "1h",
+  content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        Asperiores, quod. Lorem ipsum, dolor sit amet consectetur adipisicing
+        elit. Eligendi, omnis! Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Et, at!`,
+  likes: "30",
+  comments: [
+    {
+      _id: "fasdv",
+      username: "user",
+      image_url: "uploads/defualt.png",
+      created_at: "2h",
+      content: "stupid",
+      likes: "2",
+    },
+    {
+      _id: "agfrtrg",
+      username: "user 2",
+      image_url: "uploads/defualt.png",
+      created_at: "2h",
+      content: "Hello",
+      likes: "12",
+    },
+  ],
+}
 export default PostFeed
