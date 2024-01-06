8d574dd871bc8ea8f2b08ecae0ec291c7f337e52
AliHakim773 <AliHakim773@gmail.com>
2024-01-06 14:54:44 +0200

    Frontend: changed register form styles

--- tulp-frontend/src/components/RegisterForm/index.jsx
+++ tulp-frontend/src/components/RegisterForm/index.jsx
@@ -13,37 +13,39 @@ const RegisterForm = () => {
 
   return (
     <section className='register-section w-100 flex column center'>
-      <h2 className='semmi-bold'>Register here</h2>
-      <form className='register-form'>
-        <div className='register-inputs'>
-          {inputs.map((input) => {
-            return (
-              <InputField
-                text={input.text}
-                id={input.id}
-                type={input.type}
-                placeholder={input.placeholder}
-                key={input.id}
-                value={input.value}
-                handleOnChange={HandleOnInputChange}
-              />
-            )
-          })}
-          <RoleSelect onChange={HandleOnInputChange} />
-          <Error msg={error.msg} hidden={error.hidden} />
-        </div>
-        <div className='register-submit flex column center w-100'>
-          <div className='register-submit-btn'>
-            <SubmitButton text='Register' handleOnClick={HandleOnSubmit} />
+      <div className='form-wrapper'>
+        <h2 className='semmi-bold'>Register here</h2>
+        <form className='register-form'>
+          <div className='register-inputs'>
+            {inputs.map((input) => {
+              return (
+                <InputField
+                  text={input.text}
+                  id={input.id}
+                  type={input.type}
+                  placeholder={input.placeholder}
+                  key={input.id}
+                  value={input.value}
+                  handleOnChange={HandleOnInputChange}
+                />
+              )
+            })}
+            <RoleSelect onChange={HandleOnInputChange} />
+            <Error msg={error.msg} hidden={error.hidden} />
           </div>
-          <span className='note'>
-            Have an account?{" "}
-            <Link className='link' to={"/login"}>
-              Login
-            </Link>
-          </span>
-        </div>
-      </form>
+          <div className='register-submit flex column center w-100'>
+            <div className='register-submit-btn'>
+              <SubmitButton text='Register' handleOnClick={HandleOnSubmit} />
+            </div>
+            <span className='note'>
+              Have an account?{" "}
+              <Link className='link' to={"/login"}>
+                Login
+              </Link>
+            </span>
+          </div>
+        </form>
+      </div>
     </section>
   )
 }
