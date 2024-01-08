5ebfaad3e6aba36facb3ac76cd6798c232165c99
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 12:38:58 +0200

    Frontend: added uselogic cutom hook for post feed

---
+++ tulp-frontend/src/components/LandingFeed/PostFeed/usePostFeedLogic.js
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { postAPI } from "../../../core/api/post"
+
+const usePostFeedLogic = () => {
+  const [posts, setPosts] = useState([LoadingPost])
+  useEffect(() => {
+    const getPosts = async () => {
+      const res = await postAPI.get()
+      setPosts(res.posts)
+    }
+    getPosts()
+  }, [])
+
+  return { posts }
+}
+
+const LoadingPost = {
+  _id: "none",
+  user: {
+    _id: "none",
+    username: "Loading",
+    imageUrl: "defualt.jpg",
+  },
+  likes: ["asdadas"],
+  content: "Loading",
+  comments: [
+    {
+      _id: "none",
+      user: {
+        _id: "none",
+        username: "Loading",
+        imageUrl: "defualt.jpg",
+      },
+      likes: ["asdadas"],
+      content: "Loading",
+    },
+  ],
+}
+
+export default usePostFeedLogic
