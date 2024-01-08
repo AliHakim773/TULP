b292fec580c81336593c257bcd32de216ba00244
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 12:38:40 +0200

    Frontend: added custom hook to get posts

--- tulp-frontend/src/components/LandingFeed/PostFeed/index.jsx
+++ tulp-frontend/src/components/LandingFeed/PostFeed/index.jsx
@@ -1,41 +1,18 @@
 import React from "react"
 import "./styles.css"
 import PostItem from "../PostItem"
+import usePostFeedLogic from "./usePostFeedLogic"
 
 const PostFeed = () => {
+  const { posts } = usePostFeedLogic()
+
   return (
     <div className='post-feed'>
-      <PostItem post={test_post} />
+      {posts.map((post) => (
+        <PostItem key={post._id} post={post} />
+      ))}
     </div>
   )
 }
-const test_post = {
-  _id: "asdad",
-  username: "ali",
-  image_url: "uploads/defualt.png",
-  created_at: "1h",
-  content: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Asperiores, quod. Lorem ipsum, dolor sit amet consectetur adipisicing
-        elit. Eligendi, omnis! Lorem ipsum dolor sit amet consectetur
-        adipisicing elit. Et, at!`,
-  likes: "30",
-  comments: [
-    {
-      _id: "fasdv",
-      username: "user",
-      image_url: "uploads/defualt.png",
-      created_at: "2h",
-      content: "stupid",
-      likes: "2",
-    },
-    {
-      _id: "agfrtrg",
-      username: "user 2",
-      image_url: "uploads/defualt.png",
-      created_at: "2h",
-      content: "Hello",
-      likes: "12",
-    },
-  ],
-}
+
 export default PostFeed
