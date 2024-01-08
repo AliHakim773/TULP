a49ecc7b514a4196447a32e57f57ecb48543c173
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 19:34:40 +0200

    Frontend: added class card styles

--- tulp-frontend/src/components/ClassCard/index.jsx
+++ tulp-frontend/src/components/ClassCard/index.jsx
@@ -3,9 +3,11 @@ import "./styles.css"
 
 const ClassCard = ({ owner, name, description }) => {
   return (
-    <div className='class-card'>
+    <div className='class-card flex column shadow rounded-2'>
       <div className='class-card-header flex column'>
-        <span>By: {owner}</span>
+        <p>
+          By: <span className='blue-3-txt semi-bold'>{owner}</span>
+        </p>
         <h4>{name}</h4>
       </div>
       <div className='class-card-body'>{description}</div>
