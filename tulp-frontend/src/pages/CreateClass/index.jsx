41f721727714a1b18b2c08a8c2bd7e3318e39231
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 19:56:32 +0200

    Frontend: added craeteClassForm componant

--- tulp-frontend/src/pages/CreateClass/index.jsx
+++ tulp-frontend/src/pages/CreateClass/index.jsx
@@ -2,13 +2,19 @@ import React from "react"
 import "./styles.css"
 import BasicLayout from "../../components/UI/BasicLayout"
 import ProfileCard from "../../components/ProfileCard"
+import CreateClassForm from "../../components/CreateClassForm"
 
 const CreateClass = () => {
   return (
     <BasicLayout>
-      <div className='profile-section w-100 flex'>
-        <div className='main-profile w-100 flex'>
-          <ProfileCard />
+      <div className='create-class-section w-100 flex'>
+        <div className='create-class-main w-100 flex'>
+          <aside>
+            <ProfileCard />
+          </aside>
+          <section className='w-100'>
+            <CreateClassForm />
+          </section>
         </div>
       </div>
     </BasicLayout>
