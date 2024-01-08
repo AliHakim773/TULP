0f7e3a1d6f09f682cb27aa19875a56473639d5f3
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 12:55:20 +0200

    Frontend: connected logic

--- tulp-frontend/src/components/LandingFeed/PostField/index.jsx
+++ tulp-frontend/src/components/LandingFeed/PostField/index.jsx
@@ -1,19 +1,30 @@
 import React from "react"
 import "./styles.css"
 import Button from "../../Base/Button"
+import usePostFieldLogic from "./usePostFieldLogic"
 
 const PostField = () => {
+  const { postRef, handleonClick } = usePostFieldLogic()
   return (
     <div className='post-field'>
       <form className='flex column'>
         <div className='post-btn'>
-          <Button text='Post' type='submit' color='blue' />
+          <Button
+            text='Post'
+            type='submit'
+            color='blue'
+            onclick={(e) => {
+              e.preventDefault()
+              handleonClick()
+            }}
+          />
         </div>
         <textarea
           className='post-field-input border'
           name='post'
           id='post'
           placeholder='Type Here'
+          ref={postRef}
         />
       </form>
     </div>
