3fb11bcaca0a6a248861f6ed10effa0269da960e
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 16:09:04 +0200

    Backend: added timestamps to comment schema

--- tulp-backend/models/comment.model.js
+++ tulp-backend/models/comment.model.js
@@ -1,20 +1,25 @@
 const { default: mongoose } = require("mongoose")
 
-const CommentSchema = mongoose.Schema({
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-  content: {
-    type: String,
-    require: "Cant have an empty Comment",
-  },
-  likes: [
-    {
+const CommentSchema = mongoose.Schema(
+  {
+    user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
-  ],
-})
+    content: {
+      type: String,
+      require: "Cant have an empty Comment",
+    },
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+)
 
 module.exports = CommentSchema
