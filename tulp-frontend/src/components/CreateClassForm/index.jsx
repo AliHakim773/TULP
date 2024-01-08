d06784cdab828bd4db7957acc96dce695bb81003
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 20:08:02 +0200

    Frontend: added classname and description inputs

--- tulp-frontend/src/components/CreateClassForm/index.jsx
+++ tulp-frontend/src/components/CreateClassForm/index.jsx
@@ -1,8 +1,27 @@
 import React from "react"
 import "./styles.css"
+import InputField from "../Base/InputField"
 
 const CreateClassForm = () => {
-  return <div className='w-100'>df</div>
+  return (
+    <form className='class-create-form flex column'>
+      <div className='class-name-input'>
+        <InputField
+          text='Class Name'
+          id={"classname"}
+          placeholder='Class Name'
+        />
+      </div>
+      <div className='class-description flex column'>
+        <label htmlFor='class-description'>Class Description</label>
+        <textarea
+          id='class-description'
+          className='class-description-textarea'
+          placeholder='Enter Class Description'
+        />
+      </div>
+    </form>
+  )
 }
 
 export default CreateClassForm
