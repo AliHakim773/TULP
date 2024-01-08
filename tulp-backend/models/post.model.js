0f3c6271e13c1b6d1c1cb473842c7633516c6e4e
AliHakim773 <AliHakim773@gmail.com>
2024-01-08 12:13:50 +0200

    Backend: added timestamp

--- tulp-backend/models/post.model.js
+++ tulp-backend/models/post.model.js
@@ -1,23 +1,28 @@
 const { default: mongoose } = require("mongoose")
 const CommentSchema = require("./comment.model")
 
-const PostSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-  content: {
-    type: String,
-    require: "Cant have an empty Post",
-  },
-  likes: [
-    {
+const PostSchema = new mongoose.Schema(
+  {
+    user: {
       type: mongoose.Types.ObjectId,
       ref: "User",
     },
-  ],
-  comments: [CommentSchema],
-})
+    content: {
+      type: String,
+      require: "Cant have an empty Post",
+    },
+    likes: [
+      {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [CommentSchema],
+  },
+  {
+    timestamps: true,
+  }
+)
 
 const Post = mongoose.model("Post", PostSchema)
 
