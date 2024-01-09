5ad2b5f52a9092b023cbca44d87aa64641777544
AliHakim773 <AliHakim773@gmail.com>
2024-01-09 10:44:15 +0200

    Frontend: added instructor Item

---
+++ tulp-frontend/src/components/CreateClassForm/InstructorItem/inde.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+import "./styles.css"
+
+const InstructorItem = ({ username }) => {
+  return <div className='instructor-item'>{username}</div>
+}
+
+export default InstructorItem
