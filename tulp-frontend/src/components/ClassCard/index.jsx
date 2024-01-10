645d6606aef46473edc87b1fe0531351a390d784
AliHakim773 <AliHakim773@gmail.com>
2024-01-10 19:19:47 +0200

    Frontend: changed styling

--- tulp-frontend/src/components/ClassCard/index.jsx
+++ tulp-frontend/src/components/ClassCard/index.jsx
@@ -5,8 +5,8 @@ const ClassCard = ({ owner, name, description }) => {
   return (
     <div className='class-card flex column shadow rounded-2'>
       <div className='class-card-header flex column'>
-        <p>
-          By: <span className='blue-3-txt semi-bold'>{owner}</span>
+        <p className='semi-bold'>
+          By: <span className='blue-3-txt bold'>{owner}</span>
         </p>
         <h4>{name}</h4>
       </div>
