aa03ea028781cd53140124e19d873b9147a7133f
AliHakim773 <AliHakim773@gmail.com>
2024-01-10 14:36:56 +0200

    Frontend: changed instructor item styles

--- tulp-frontend/src/components/CreateClassForm/InstructorItem/index.jsx
+++ tulp-frontend/src/components/CreateClassForm/InstructorItem/index.jsx
@@ -4,10 +4,16 @@ import "./styles.css"
 const InstructorItem = ({ instructor, onAccept }) => {
   return (
     <div
-      className='instructor-item'
+      className='instructor-item flex'
       onClick={() => {
         onAccept(instructor)
       }}>
+      <div className='instructor-img'>
+        <img
+          src={`http://localhost:8000/${instructor.imageUrl}`}
+          alt={instructor.username}
+        />
+      </div>
       {instructor.username}
     </div>
   )
