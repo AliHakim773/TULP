079082786f5f9a1f38ef0b03f6222207bee81d0e
AliHakim773 <AliHakim773@gmail.com>
2024-01-10 15:37:11 +0200

    Frontend: added add class api call

--- tulp-frontend/src/core/api/class.js
+++ tulp-frontend/src/core/api/class.js
@@ -1,6 +1,14 @@
 import { sendRequest } from "../axios"
 
 const classAPI = {
+  add: async (data) => {
+    const response = await sendRequest({
+      route: "class",
+      method: "POST",
+      body: data,
+    })
+    return response
+  },
   search: async (data) => {
     const response = await sendRequest({
       route: "class/search",
