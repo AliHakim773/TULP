e061f4a1633243a866fa69210653b5729fa17c65
AliHakim773 <AliHakim773@gmail.com>
2024-01-10 19:19:14 +0200

    Frontend: added logic to get class

--- tulp-frontend/src/pages/Profile/useProfileLogic.js
+++ tulp-frontend/src/pages/Profile/useProfileLogic.js
@@ -1,35 +1,42 @@
+import { useEffect, useState } from "react"
+import classAPI from "../../core/api/class"
+
+const loading_class = [
+  {
+    _id: "skdjuhf",
+    owner: { username: "Ali" },
+    name: "Computer Bro",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
+  },
+  {
+    _id: "skdjasdasduhf",
+    owner: { username: "Mohamad" },
+    name: "Electrical Bro",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
+  },
+  {
+    _id: "skdjasddasdaasduhf",
+    owner: { username: "Nour" },
+    name: "English Bro",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
+  },
+]
+
 const useProfileLogic = () => {
-  const test_class = [
-    {
-      _id: "skdjuhf",
-      owner: "Ali",
-      name: "Computer Bro",
-      descriptiom:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
-    },
-    {
-      _id: "skdjasdasduhf",
-      owner: "Mohamad",
-      name: "Electrical Bro",
-      descriptiom:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
-    },
-    {
-      _id: "skdjasddasdaasduhf",
-      owner: "Nour",
-      name: "English Bro",
-      descriptiom:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
-    },
-    {
-      _id: "skdjaasdaddasdaasduhf",
-      owner: "Fadia",
-      name: "Pharmacy Bro",
-      descriptiom:
-        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
-    },
-  ]
-  return { test_class }
+  const [classes, setClasses] = useState([])
+  useEffect(() => {
+    const getClasses = async () => {
+      const res = await classAPI.getClassesIn()
+      setClasses(res.classes)
+      console.log(res.classes)
+    }
+    getClasses()
+  }, [])
+
+  return { classes }
 }
 
 export default useProfileLogic
