a03ca478c367090e789489c42b2b9a2ccc719874
AliHakim773 <AliHakim773@gmail.com>
2024-01-11 16:50:12 +0200

    Backend: added extenstion check

--- tulp-backend/middlewares/multer.middleware.js
+++ tulp-backend/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
 const multer = require("multer")
+const path = require("path")
 
 const upload = multer({
   storage: multer.diskStorage({
@@ -6,9 +7,19 @@ const upload = multer({
       cb(null, "uploads/")
     },
     filename: (req, file, cb) => {
-      cb(null, file.fieldname + "-" + Date.now() + ".png")
+      cb(
+        null,
+        file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+      )
     },
   }),
+  fileFilter: (req, file, cb) => {
+    const ext = path.extname(file.originalname)
+    if (ext !== ".png" && ext !== ".jpg" && ext !== ".jpeg") {
+      return cb(new Error(`${ext} is not supported`))
+    }
+    cb(null, true)
+  },
 })
 
 module.exports = upload
