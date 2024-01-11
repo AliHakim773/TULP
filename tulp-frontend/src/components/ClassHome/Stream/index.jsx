75c9f5a24eb5bafc9a7b12302ea672b3dd94dabe
AliHakim773 <AliHakim773@gmail.com>
2024-01-11 16:51:17 +0200

    Frontend: added header for class Stream

--- tulp-frontend/src/components/ClassHome/Stream/index.jsx
+++ tulp-frontend/src/components/ClassHome/Stream/index.jsx
@@ -1,7 +1,14 @@
+import Button from "../../Base/Button"
 import "./styles.css"
 
 const Stream = () => {
-  return <div></div>
+  return (
+    <div className='class-stream flex column w-100'>
+      <div className='class-stream-header'>
+        <Button text='Post' />
+      </div>
+    </div>
+  )
 }
 
 export default Stream
