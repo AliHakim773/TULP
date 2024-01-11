b483dee3c5dc8e69fedf487e0933354e373befef
AliHakim773 <AliHakim773@gmail.com>
2024-01-11 14:07:48 +0200

    Frontend: Changed the createBrowserRouter inputs from json to Elements for better readability

--- tulp-frontend/src/core/hooks/useAppRoutes.jsx
+++ tulp-frontend/src/core/hooks/useAppRoutes.jsx
@@ -7,53 +7,30 @@ import EditProfile from "../../pages/EditProfile"
 import Profile from "../../pages/Profile"
 import CreateClass from "../../pages/CreateClass"
 import ClassHomePage from "../../pages/ClassHomePage"
-import { createBrowserRouter } from "react-router-dom"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom"
 
 const useAppRoutes = () => {
-  const router = createBrowserRouter([
-    {
-      element: <BasicLayout />,
-      children: [
-        {
-          path: "/",
-          element: <LandingPage />,
-        },
-        {
-          path: "/register",
-          element: <RegisterPage />,
-        },
-        {
-          path: "/login",
-          element: <LoginPage />,
-        },
-        {
-          path: "/profile/:id?",
-          element: <Profile />,
-        },
-        {
-          path: "/edit-profile",
-          element: <EditProfile />,
-        },
-        {
-          path: "/create-class",
-          element: <CreateClass />,
-        },
-      ],
-    },
-    {
-      element: <BasicLayout />,
-      children: [
-        {
-          path: "/class/:slug",
-          element: <ClassHomePage />,
-        },
-      ],
-    },
-    {
-      path: "/*",
-      element: <PageNotFound />,
-    },
-  ])
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/'>
+        <Route element={<BasicLayout />}>
+          <Route index element={<LandingPage />} />
+          <Route path='register' element={<RegisterPage />} />
+          <Route path='login' element={<LoginPage />} />
+          <Route path='profile/:id?' element={<Profile />} />
+          <Route path='edit-profile' element={<EditProfile />} />
+          <Route path='create-class' element={<CreateClass />} />
+        </Route>
+        <Route path='class/' element={<BasicLayout />}>
+          <Route path=':slug' element={<ClassHomePage />} />
+        </Route>
+      </Route>
+    )
+  )
   return { router }
 }
 
