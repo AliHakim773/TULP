1c965fe3ab70ec0b13a70ccbffbd5ab495848b86
AliHakim773 <AliHakim773@gmail.com>
2024-01-13 13:27:57 +0200

    Frontend: added link to assignment view page

--- tulp-frontend/src/components/ClassHome/AssignmentItem/index.jsx
+++ tulp-frontend/src/components/ClassHome/AssignmentItem/index.jsx
@@ -1,14 +1,18 @@
+import { Link, useParams } from "react-router-dom"
 import "./styles.css"
 
 const AssignmentItem = () => {
+  const { slug } = useParams()
   return (
-    <div className='stream-item assignment-item w-100 flex column border rounded-1'>
+    <Link
+      to={`/class/${slug}/assignments/asd`}
+      className='stream-item assignment-item w-100 flex column border rounded-1'>
       <div className='stream-header flex '>
         <div className='stream-title'>Title</div>
         <div className='stream-due grey-2-txt'>Due: jan 11 7:00 P.M.</div>
       </div>
       <div className='stream-content'>Some Details</div>
-    </div>
+    </Link>
   )
 }
 
