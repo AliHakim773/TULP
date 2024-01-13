8cd538df7ea90a6f08a0629a66c090395dc32a65
AliHakim773 <AliHakim773@gmail.com>
2024-01-13 12:56:24 +0200

    Frontend: added assignment

--- tulp-frontend/src/components/ClassHome/Assignments/index.jsx
+++ tulp-frontend/src/components/ClassHome/Assignments/index.jsx
@@ -1,7 +1,20 @@
+import Button from "../../Base/Button"
+import AssignmentItem from "../AssignmentItem"
 import "./styles.css"
 
 const Assignments = () => {
-  return <div></div>
+  return (
+    <div className='class-assignment flex column w-100'>
+      <div className='class-assignment-header'>
+        <Button text='Post' />
+      </div>
+      <div className='assignment-main flex column w-100'>
+        {/* {test_post.map((post) => { */}
+        <AssignmentItem />
+        {/*  })} */}
+      </div>
+    </div>
+  )
 }
 
 export default Assignments
