3ce608e3070548d44ee30ee318e3aeb84fe4e9a4
AliHakim773 <AliHakim773@gmail.com>
2024-01-13 10:17:15 +0200

    Frontend: populated class Stream with mock data

--- tulp-frontend/src/components/ClassHome/Stream/index.jsx
+++ tulp-frontend/src/components/ClassHome/Stream/index.jsx
@@ -2,14 +2,33 @@ import Button from "../../Base/Button"
 import StreamItem from "../StreamItem"
 import "./styles.css"
 
+const test_post = [
+  {
+    title: "Post 1",
+    content:
+      "Hello This is a Post Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tempora, voluptas tempore aliquam praesentium consequuntur asperiores omnis magnam ab iste minus adipisci ea nesciunt nobis aut dolor? Nulla, libero officia!",
+    fileLink: "uploads/files/Ali_Hakim_CV_v0.docx",
+    id: "sfgdsfdgsdabgg",
+  },
+  {
+    title: "Post 2",
+    content:
+      "Hello This is another Post Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia tempora, voluptas tempore aliquam praesentium consequuntur asperiores omnis magnam ab iste minus adipisci ea nesciunt nobis aut dolor? Nulla, libero officia!",
+    fileLink: "uploads/files/Ali_Hakim_CV.pdf",
+    id: "sfgdsfdgsdasddabgg",
+  },
+]
+
 const Stream = () => {
   return (
     <div className='class-stream flex column w-100'>
       <div className='class-stream-header'>
         <Button text='Post' />
       </div>
-      <div className='stream-main w-100'>
-        <StreamItem />
+      <div className='stream-main flex column w-100'>
+        {test_post.map((post) => {
+          return <StreamItem key={post.id} post={post} />
+        })}
       </div>
     </div>
   )
--- tulp-frontend/src/components/ClassHome/StreamItem/index.jsx
+++ tulp-frontend/src/components/ClassHome/StreamItem/index.jsx
@@ -1,17 +1,13 @@
 import "./styles.css"
 import file from "../../../assets/svgs/file.svg"
 
-const StreamItem = () => {
+const StreamItem = ({ post }) => {
+  const { title, content, fileLink, id } = post
   return (
     <div className='stream-item w-100 flex border rounded-1'>
       <div className='stream-body'>
-        <div className='stream-title'>Post 1</div>
-        <div className='stream-content'>
-          Hello This is a Post Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Officia tempora, voluptas tempore aliquam
-          praesentium consequuntur asperiores omnis magnam ab iste minus
-          adipisci ea nesciunt nobis aut dolor? Nulla, libero officia!
-        </div>
+        <div className='stream-title'>{title}</div>
+        <div className='stream-content'>{content}</div>
       </div>
       <div className='stream-files'>
         <div className='file-img'>
@@ -19,7 +15,7 @@ const StreamItem = () => {
         </div>
         <a
           target='_blank'
-          href={`http://localhost:8000/uploads/files/Ali_Hakim_CV_v0.docx`}
+          href={`http://localhost:8000/${fileLink}`}
           className='download-btn'>
           Download
         </a>
