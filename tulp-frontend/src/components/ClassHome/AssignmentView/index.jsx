23c596c746343d0d48c1193f31aac56ed0ce167a
AliHakim773 <AliHakim773@gmail.com>
2024-01-13 13:28:18 +0200

    Frontend: completed assignment view layout

--- tulp-frontend/src/components/ClassHome/AssignmentView/index.jsx
+++ tulp-frontend/src/components/ClassHome/AssignmentView/index.jsx
@@ -1,7 +1,33 @@
+import { useParams } from "react-router-dom"
+import file from "../../../assets/svgs/file.svg"
 import "./styles.css"
 
 const AssignmentView = () => {
-  return <div></div>
+  const { titleSlug } = useParams()
+  return (
+    <div className='assignment-view w-100 flex border rounded-1'>
+      <div className='assignment-body'>
+        <div className='title'>asd</div>
+        <p>Some content Here</p>
+        <div className='assignment-file flex column center'>
+          <div className='file-img'>
+            <img src={file} alt='file' />
+          </div>
+          <a
+            target='_blank'
+            href={`http://localhost:8000/`}
+            className='download-btn'>
+            Download
+          </a>
+        </div>
+      </div>
+      <div className='assignment-upload flex column'>
+        <div className='stream-due grey-2-txt'>Due: jan 11 7:00 P.M.</div>
+        <a className='download-btn'>Upload File</a>
+        <span>Submited</span>
+      </div>
+    </div>
+  )
 }
 
 export default AssignmentView
