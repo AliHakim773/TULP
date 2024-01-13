fe8dc1e95e462824f3cb034376a3750bf734dbd5
AliHakim773 <AliHakim773@gmail.com>
2024-01-13 17:04:49 +0200

    Frontend: aded schedule

--- tulp-frontend/src/components/ClassHome/Schedule/index.jsx
+++ tulp-frontend/src/components/ClassHome/Schedule/index.jsx
@@ -1,9 +1,31 @@
-import './styles.css'
+import { Outlet, useNavigate, useParams } from "react-router-dom"
+import Button from "../../Base/Button"
+import ScheduleItem from "../ScheduleItem"
+import "./styles.css"
 
 const Schedule = () => {
-    return (
-        <div></div>
-    )
+  const navigate = useNavigate()
+  const { slug } = useParams()
+
+  return (
+    <>
+      <Outlet />
+      <div className='schedule'>
+        <div className='schedule-header flex'>
+          <Button
+            text='Add'
+            onclick={() => {
+              navigate(`add`)
+            }}
+          />
+          <Button text='Enter' />
+        </div>
+        <ScheduleItem />
+        <ScheduleItem />
+        <ScheduleItem />
+      </div>
+    </>
+  )
 }
 
 export default Schedule
