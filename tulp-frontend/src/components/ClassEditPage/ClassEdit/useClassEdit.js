0974c096e5e1792c3f2ef9d82b7bb3ff4f518029
AliHakim773 <AliHakim773@gmail.com>
2024-01-15 15:14:56 +0200

    Frontend: added editClassProfile call on click

--- tulp-frontend/src/components/ClassEditPage/ClassEdit/useClassEdit.js
+++ tulp-frontend/src/components/ClassEditPage/ClassEdit/useClassEdit.js
@@ -1,11 +1,21 @@
 import { useState } from "react"
-import { useLoaderData } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom"
+import classAPI from "../../../core/api/class"
+import toast from "react-hot-toast"
 
 const useClassEdit = () => {
+  const { slug } = useParams()
   const data = useLoaderData()
   const [values, setValues] = useState(data)
   const handleSubmit = async () => {
     console.log(values)
+    try {
+      const res = await classAPI.editClassProfile(slug, values)
+      toast.success("Class Information Updated")
+    } catch (e) {
+      console.log(e)
+      toast.error("There was an error")
+    }
   }
   const handleOnChange = (e) => {
     setValues((prev) => {
