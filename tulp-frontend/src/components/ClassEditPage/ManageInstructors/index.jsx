1d95892638b3d144f445c955d4ab39f509fdb90c
AliHakim773 <AliHakim773@gmail.com>
2024-01-15 16:03:05 +0200

    Frontend: added instructors loader function

--- tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
@@ -1,7 +1,15 @@
+import { useLoaderData } from "react-router-dom"
+import classAPI from "../../../core/api/class"
 import "./styles.css"
 
 const ManageInstructors = () => {
-  return <div></div>
+  const data = useLoaderData()
+  return <div className='w-100'>{JSON.stringify(data)}</div>
 }
 
 export default ManageInstructors
+
+export const manageInstructorsLoader = async ({ params }) => {
+  const res = await classAPI.getClassInstructors(params.slug)
+  return res
+}
