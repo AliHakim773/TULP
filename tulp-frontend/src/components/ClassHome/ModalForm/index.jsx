937045bd7f42830e01e089bedcb17aac1c1f50a6
AliHakim773 <AliHakim773@gmail.com>
2024-01-15 10:13:48 +0200

    Frontend: added modal form

--- tulp-frontend/src/components/ClassHome/ModalForm/index.jsx
+++ tulp-frontend/src/components/ClassHome/ModalForm/index.jsx
@@ -1,7 +1,20 @@
+import { Outlet, useNavigate } from "react-router-dom"
 import "./styles.css"
 
 const ModalForm = () => {
-  return <div className='modal-form'></div>
+  const navigate = useNavigate()
+  return (
+    <div
+      id='modal'
+      className='modal-form flex center'
+      onClick={(e) => {
+        if (e.target.id === "modal") {
+          navigate(-1)
+        }
+      }}>
+      <Outlet />
+    </div>
+  )
 }
 
 export default ModalForm
