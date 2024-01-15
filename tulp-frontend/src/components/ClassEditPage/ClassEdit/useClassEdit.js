b962ec64521cff91fd9456fc429154d9dc626604
AliHakim773 <AliHakim773@gmail.com>
2024-01-15 15:36:32 +0200

    Frontend: fixed editClassProfile function

--- tulp-frontend/src/components/ClassEditPage/ClassEdit/useClassEdit.js
+++ tulp-frontend/src/components/ClassEditPage/ClassEdit/useClassEdit.js
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { useLoaderData, useParams } from "react-router-dom"
+import { useLoaderData, useNavigate, useParams } from "react-router-dom"
 import classAPI from "../../../core/api/class"
 import toast from "react-hot-toast"
 
@@ -7,16 +7,19 @@ const useClassEdit = () => {
   const { slug } = useParams()
   const data = useLoaderData()
   const [values, setValues] = useState(data)
+  const navigate = useNavigate()
+
   const handleSubmit = async () => {
-    console.log(values)
     try {
       const res = await classAPI.editClassProfile(slug, values)
       toast.success("Class Information Updated")
+      console.log(res.slug)
+      navigate(`/class/${res.slug}/edit`)
     } catch (e) {
-      console.log(e)
       toast.error("There was an error")
     }
   }
+
   const handleOnChange = (e) => {
     setValues((prev) => {
       return { ...prev, [e.target.id]: e.target.value }
