a53205c6cc168ad5039d8fdb77d94ae04a33b594
AliHakim773 <AliHakim773@gmail.com>
2024-01-16 17:02:01 +0200

    Frontend: removed console.log

--- tulp-frontend/src/pages/Profile/useProfileLogic.js
+++ tulp-frontend/src/pages/Profile/useProfileLogic.js
@@ -1,6 +1,21 @@
 import { useEffect, useState } from "react"
 import classAPI from "../../core/api/class"
 
+const useProfileLogic = () => {
+  const [classes, setClasses] = useState([])
+  useEffect(() => {
+    const getClasses = async () => {
+      const res = await classAPI.getClassesIn()
+      setClasses(res.classes)
+    }
+    getClasses()
+  }, [])
+
+  return { classes }
+}
+
+export default useProfileLogic
+
 const loading_class = [
   {
     _id: "skdjuhf",
@@ -24,19 +39,3 @@ const loading_class = [
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam eius eaque corporis consequuntur rem numquam similique nesciunt non, autem quasi labore quibusdam dolorum. Quia libero nostrum, quod nemo minus ullam ad odit amet repellat repellendus. Voluptatem tempore aliquam quasi qui?",
   },
 ]
-
-const useProfileLogic = () => {
-  const [classes, setClasses] = useState([])
-  useEffect(() => {
-    const getClasses = async () => {
-      const res = await classAPI.getClassesIn()
-      setClasses(res.classes)
-      console.log(res.classes)
-    }
-    getClasses()
-  }, [])
-
-  return { classes }
-}
-
-export default useProfileLogic
