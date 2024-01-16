9cdcc6affe3e17675018890b8b1e9e666312ea52
AliHakim773 <AliHakim773@gmail.com>
2024-01-16 14:37:35 +0200

    Frontend: added addClassInstructor

--- tulp-frontend/src/components/ClassEditPage/ManageInstructors/AddInstructorForm/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageInstructors/AddInstructorForm/index.jsx
@@ -1,11 +1,22 @@
-import { useNavigate } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import SearchInstructor from "../../../SearchInstructor"
 import "./styles.css"
+import toast from "react-hot-toast"
+import classAPI from "../../../../core/api/class"
 const AddInstructorForm = () => {
   const navigate = useNavigate()
+  const { slug } = useParams()
   const handleOnAccept = async (instructor) => {
     console.log(instructor)
     // TODO: Add the instructor
+    try {
+      const res = await classAPI.addClassInstructors(slug, {
+        instructorId: instructor._id,
+      })
+      toast.success(res.message)
+    } catch (e) {
+      toast.error("There was an error")
+    }
     navigate(-1)
   }
   return (
