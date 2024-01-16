cd495429818325e96676c26b472a4ad609b7c238
AliHakim773 <AliHakim773@gmail.com>
2024-01-16 17:01:16 +0200

    Frontend: added loader to class home page

--- tulp-frontend/src/pages/ClassHomePage/classLoader.js
+++ tulp-frontend/src/pages/ClassHomePage/classLoader.js
@@ -1,3 +1,18 @@
-const classLoader = async ({ param }) => {
-  return
+import { redirect } from "react-router-dom"
+import classAPI from "../../core/api/class"
+import { store } from "../../core/redux/store"
+
+export const classLoader = async ({ params }) => {
+  const { _id } = store.getState().user
+  const res = await classAPI.get(params.slug)
+  const isInstructor = res.class.instructors.some((instructor) => {
+    return instructor._id === _id
+  })
+  const isStudnet = res.class.students.some((student) => {
+    return student._id === _id
+  })
+  if (res.class.owner._id === _id || isInstructor || isStudnet) {
+    return { res }
+  }
+  return redirect("/profile")
 }
