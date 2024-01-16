38216adf8911cb755d8be784775f2f9baa3ccce0
AliHakim773 <AliHakim773@gmail.com>
2024-01-16 12:17:45 +0200

    Frontend: added add button to add instructors

--- tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
@@ -1,20 +1,29 @@
-import { useLoaderData } from "react-router-dom"
-import classAPI from "../../../core/api/class"
+import { Outlet, useNavigate } from "react-router-dom"
 import "./styles.css"
 import Table from "../../UI/Table"
+import Button from "../../Base/Button"
+import useManageInstructors from "./useManageInstructors"
 
 const ManageInstructors = () => {
-  const data = useLoaderData()
+  const { data } = useManageInstructors()
+  const navigate = useNavigate()
+
   return (
     <div className='instructors-table'>
+      <Outlet />
+      <div className='instructors-header'>
+        <div className='instructors-add-btn flex'>
+          <Button
+            text='Add'
+            onclick={() => {
+              navigate("add")
+            }}
+          />
+        </div>
+      </div>
       <Table users={data.instructors} />
     </div>
   )
 }
 
 export default ManageInstructors
-
-export const manageInstructorsLoader = async ({ params }) => {
-  const res = await classAPI.getClassInstructors(params.slug)
-  return res
-}
