c23bc7406b04a05a47556faf26085e87e9eaf573
AliHakim773 <AliHakim773@gmail.com>
2024-01-16 17:15:37 +0200

    Frontend: added props to UserList component

--- tulp-frontend/src/components/UserList/index.jsx
+++ tulp-frontend/src/components/UserList/index.jsx
@@ -1,31 +1,27 @@
 import React from "react"
 import "./styles.css"
 
-const UserList = ({ title, users }) => {
+const UserList = ({ title = "title", users }) => {
   return (
     <div className='user-list w-100 shadow rounded-2'>
-      <h4>User</h4>
+      <h4>{title}</h4>
       <div className='users flex column'>
-        <div className='user-item flex'>
-          <div className='user-item-name'>username</div>
-          <div className='user-item-pfp circle'>
-            <img
-              className='circle'
-              src='http://localhost:8000/uploads/default.png'
-              alt='username'
-            />
-          </div>
-        </div>
-        <div className='user-item flex'>
-          <div className='user-item-name'>username</div>
-          <div className='user-item-pfp circle'>
-            <img
-              className='circle'
-              src='http://localhost:8000/uploads/default.png'
-              alt='username'
-            />
-          </div>
-        </div>
+        {users && users.length !== 0
+          ? users.map((user) => {
+              return (
+                <div className='user-item flex'>
+                  <div className='user-item-name'>{user.username}</div>
+                  <div className='user-item-pfp circle'>
+                    <img
+                      className='circle'
+                      src={`http://localhost:8000/${user.imageUrl}`}
+                      alt={user.username}
+                    />
+                  </div>
+                </div>
+              )
+            })
+          : `No ${title}`}
       </div>
     </div>
   )
