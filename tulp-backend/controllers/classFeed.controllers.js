58555280d7d9c8d614e3173e52981c25c676933b
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 13:39:19 +0200

    Backend: added postToFeed api function

---
+++ tulp-backend/controllers/classFeed.controllers.js
@@ -0,0 +1,26 @@
+const Class = require("../models/class.model")
+
+const postToFeed = async (req, res) => {
+  const { slug } = req.params
+  const filePath = req.file.path
+  const { title, content } = req.body
+
+  try {
+    const classObject = await Class.findOne({ slug })
+
+    const post = {
+      title,
+      content,
+      file: filePath,
+    }
+
+    classObject.classFeed.push(post)
+    await classObject.save()
+
+    return res.status(200).send({ class: classObject })
+  } catch (error) {
+    return res.status(500).send({ error })
+  }
+}
+
+module.exports = { postToFeed }
