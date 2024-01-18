a6471037483155890d6c9618455ffc79e6f81873
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 19:05:59 +0200

    Backend: added assignment functions

--- tulp-backend/controllers/classFeed.controllers.js
+++ tulp-backend/controllers/classFeed.controllers.js
@@ -1,3 +1,5 @@
+const formatDateTime = require("../helpers/formatDateTime")
+const Assignment = require("../models/assignment.model")
 const Class = require("../models/class.model")
 
 const postToFeed = async (req, res) => {
@@ -33,4 +35,99 @@ const getClassFeed = async (req, res) => {
   }
 }
 
-module.exports = { postToFeed, getClassFeed }
+const addAssignment = async (req, res) => {
+  const { slug } = req.params
+  let filePath = ""
+  if (req.file) {
+    filePath = req.file.path
+  }
+  const { title, content, dueDate } = req.body
+  const formatedDueDate = formatDateTime(dueDate)
+  try {
+    const classObject = await Class.findOne({ slug }).populate("assignments")
+
+    if (classObject.assignments.some((ass) => ass.title === title)) {
+      return res.status(400).send({ message: "title already exist" })
+    }
+
+    const assignment = await Assignment.create({
+      title,
+      content,
+      dueDate: formatedDueDate,
+      file: filePath,
+    })
+
+    classObject.assignments.push(assignment)
+    await classObject.save()
+
+    return res.status(200).send({ class: classObject })
+  } catch (error) {
+    return res.status(500).send({ message: error })
+  }
+}
+
+const getClassAssignments = async (req, res) => {
+  const { slug } = req.params
+  try {
+    const classObject = await Class.findOne({ slug }).populate("assignments")
+    return res.status(200).send({ assignments: classObject.assignments })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+
+const getAssignment = async (req, res) => {
+  const { slug } = req.params
+  try {
+    const assignment = await Assignment.findOne({ slug })
+    return res.status(200).send({ assignment: assignment })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+
+const submitAssignment = async (req, res) => {
+  const { _id } = req.user
+  const { assignmentId } = req.params
+  if (!req.file) {
+    return res.status(400).send({ message: "You need to upload a file" })
+  }
+  const filePath = req.file.path
+
+  try {
+    const assignment = await Assignment.findById(assignmentId)
+
+    const existingAssignment = await Assignment.findOne({
+      _id: assignmentId,
+      submissions: _id,
+    })
+    if (existingAssignment) {
+      await Assignment.updateOne(
+        { _id: assignmentId },
+        { $pull: { submissions: { sender: _id } } }
+      )
+    }
+
+    const submission = await assignment.submissions.create({
+      sender: _id,
+      file: filePath,
+    })
+
+    assignment.submissions.push(submission)
+
+    await assignment.save()
+
+    res.status(200).send({ existingAssignment })
+  } catch (error) {
+    res.status(500).send({ message: error })
+  }
+}
+
+module.exports = {
+  postToFeed,
+  getClassFeed,
+  addAssignment,
+  getClassAssignments,
+  getAssignment,
+  submitAssignment,
+}
