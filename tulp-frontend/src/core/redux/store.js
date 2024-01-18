c683b822381e8dffc8bcd17f0400ef471f1358fc
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 12:53:13 +0200

    Frontend: added redux loger

--- tulp-frontend/src/core/redux/store.js
+++ tulp-frontend/src/core/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit"
 import userSlice, { user } from "./userSlice"
+import { createLogger } from "redux-logger"
+
+const logger = createLogger()
 
 export const store = configureStore({
   reducer: {
     [user]: userSlice,
   },
+  middleware: (getDefaultMiddleware) => [logger],
 })
