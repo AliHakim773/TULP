5ca1f5670a17442b6226ed62c4f8d7b65bc4848d
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 19:05:32 +0200

    Backedn: deleted unused files

--- tulp-backend/routes/assignment.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-const { addAssignment } = require("../controllers/assignment.controllers")
-const router = express.Router()
-
-router.post("/:classId", addAssignment)
-
-module.exports = router
