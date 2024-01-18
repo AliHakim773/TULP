3a601d78777ff0f353a20cfb8ded5add5866342a
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 11:20:54 +0200

    Frontend: added requests page

--- tulp-frontend/src/components/ClassEditPage/ManageRequests/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageRequests/index.jsx
@@ -1,7 +1,27 @@
+import { useLoaderData, useParams } from "react-router-dom"
 import "./styles.css"
+import Table from "../../UI/Table"
+import classAPI from "../../../core/api/class"
 
 const ManageRequests = () => {
-  return <div></div>
+  const data = useLoaderData()
+  const { slug } = useParams()
+  const accept = async (id) => {
+    await classAPI.acceptRequest(slug, { _id: id })
+  }
+  const reject = async (id) => {
+    await classAPI.rejectRequest(slug, { _id: id })
+  }
+
+  return (
+    <div className='instructors-table'>
+      <Table
+        users={data.requests}
+        handleAccept={accept}
+        handleRemove={reject}
+      />
+    </div>
+  )
 }
 
 export default ManageRequests
