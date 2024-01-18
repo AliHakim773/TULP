3eb9f9864ac4e57930c9297c9a138c15d33003ad
AliHakim773 <AliHakim773@gmail.com>
2024-01-18 11:53:01 +0200

    Frontend: adjusted table functions

--- tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageInstructors/index.jsx
@@ -5,16 +5,20 @@ import Button from "../../Base/Button"
 import useManageInstructors from "./useManageInstructors"
 import toast from "react-hot-toast"
 import classAPI from "../../../core/api/class"
+import { useState } from "react"
 
 const ManageInstructors = () => {
   const { data } = useManageInstructors()
   const { slug } = useParams()
   const navigate = useNavigate()
-  const removeInstructor = async (id) => {
+  const [insstructors, setInstructors] = useState(data.instructors)
+  const removeInstructor = async (id, users) => {
     try {
       const res = await classAPI.removeClassInstructors(slug, {
         instructorId: id,
       })
+      const ins = users.filter((user) => user._id !== id)
+      setInstructors(ins)
       toast.success(res.message)
     } catch (e) {
       toast.error("There was an error")
@@ -34,7 +38,7 @@ const ManageInstructors = () => {
           />
         </div>
       </div>
-      <Table users={data.instructors} handleRemove={removeInstructor} />
+      <Table users={insstructors} handleRemove={removeInstructor} />
     </div>
   )
 }
--- tulp-frontend/src/components/ClassEditPage/ManageRequests/index.jsx
+++ tulp-frontend/src/components/ClassEditPage/ManageRequests/index.jsx
@@ -2,24 +2,38 @@ import { useLoaderData, useParams } from "react-router-dom"
 import "./styles.css"
 import Table from "../../UI/Table"
 import classAPI from "../../../core/api/class"
+import { useState } from "react"
+import toast from "react-hot-toast"
 
 const ManageRequests = () => {
   const data = useLoaderData()
   const { slug } = useParams()
-  const accept = async (id) => {
-    await classAPI.acceptRequest(slug, { _id: id })
+  const [requests, setRequests] = useState(data.requests)
+
+  const accept = async (id, users) => {
+    try {
+      await classAPI.acceptRequest(slug, { _id: id })
+      const req = users.filter((user) => user._id !== id)
+      setRequests(req)
+      toast.success(res.message)
+    } catch (e) {
+      toast.error("There was ana error")
+    }
   }
-  const reject = async (id) => {
-    await classAPI.rejectRequest(slug, { _id: id })
+  const reject = async (id, users) => {
+    try {
+      await classAPI.rejectRequest(slug, { _id: id })
+      const req = users.filter((user) => user._id !== id)
+      setRequests(req)
+      toast.success(res.message)
+    } catch (e) {
+      toast.error("There was ana error")
+    }
   }
 
   return (
     <div className='instructors-table'>
-      <Table
-        users={data.requests}
-        handleAccept={accept}
-        handleRemove={reject}
-      />
+      <Table users={requests} handleAccept={accept} handleRemove={reject} />
     </div>
   )
 }
