9dbbb5efa588130471051f9851a3771f0202e0f5
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 14:34:08 +0200

    Frontend: added test onClick

--- tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
+++ tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
@@ -1,14 +1,24 @@
+import { useNavigate } from "react-router-dom"
 import SettingsDots from "../../Base/SettingsDots"
 
 const ChatAsideLeft = () => {
+  const navigate = useNavigate()
   return (
     <div className='border chat-aside chat-aside-left'>
-      <div className='chat-aside-nav chat-aside-item'>Back To Class</div>
+      <div
+        className='chat-aside-nav chat-aside-item'
+        onClick={() => navigate("/class/test-after/stream")}>
+        Back To Class
+      </div>
       <div className='chat-aside-header chat-aside-item'>
         Channel <SettingsDots className='chat-aside-settings' />
       </div>
-      <div className='chat-aside-item'>Some Channel</div>
-      <div className='chat-aside-item'>Some Channel</div>
+      <div className='chat-aside-item' onClick={() => navigate("general")}>
+        Some Channel
+      </div>
+      <div className='chat-aside-item' onClick={() => navigate("lol")}>
+        Some Channel
+      </div>
       <div className='chat-aside-item'>Some Channel</div>
     </div>
   )
