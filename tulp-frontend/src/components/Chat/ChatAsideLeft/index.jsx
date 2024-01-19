701f98dc40181e8f5813669f3aed75cfb9534104
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 13:26:08 +0200

    Frontend: Added basic styles for chat aside left

--- tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
+++ tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
@@ -1,7 +1,17 @@
-import "./styles.css"
+import SettingsDots from "../../Base/SettingsDots"
 
 const ChatAsideLeft = () => {
-  return <div></div>
+  return (
+    <div className='border chat-aside chat-aside-left'>
+      <div className='chat-aside-nav chat-aside-item'>Back To Class</div>
+      <div className='chat-aside-header chat-aside-item'>
+        Channel <SettingsDots className='chat-aside-settings' />
+      </div>
+      <div className='chat-aside-item'>Some Channel</div>
+      <div className='chat-aside-item'>Some Channel</div>
+      <div className='chat-aside-item'>Some Channel</div>
+    </div>
+  )
 }
 
 export default ChatAsideLeft
