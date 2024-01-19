c1cd1df375c2fe8166ad7286cba5b25d8ef59eb8
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 19:14:20 +0200

    Backend: added channel:join-room and leave-room and send-message socket events

--- tulp-backend/sockets/registerChatHandler.js
+++ tulp-backend/sockets/registerChatHandler.js
@@ -1,27 +1,67 @@
 const { sendMessage } = require("../controllers/message.controllers")
+const isUserInClass = require("../helpers/isUserInClass")
+const Channel = require("../models/channel.model")
 
-const registerChatHandler = (io, socket) => {
-  const joinRoom = (room, cb) => {
-    socket.join(room)
-    cb(`Joined ${room}`)
-    socket.emit("chat:join-room", room)
-  }
+const registerChatHandler = (io, socket, user) => {
+  socket.on("channel:join-room", async (room, cb) => {
+    const channel = await Channel.findById(room)
+    if (!channel)
+      return socket.emit("channel:join-room", false, "Channel doesnt exist")
 
-  const leaveRoom = (room, cb) => {
+    const [inClass, msg] = await isUserInClass(user._id, channel.classId)
+
+    if (!inClass) return socket.emit("channel:join-room", false, "Not In Class")
+
+    if (channel.readPermission === "all") {
+      socket.join(room)
+      return socket.emit("channel:join-room", true, "channel All")
+    }
+    if (channel.readPermission === "instructor") {
+      if (user.role !== "instructor")
+        return socket.emit("channel:join-room", false, "You Have No Access")
+
+      socket.join(room)
+      socket.emit("channel:join-room", true, "channel instructors")
+    }
+  })
+
+  socket.on("channel:leave-room", (room, cb) => {
     socket.leave(room)
     cb(`Left ${room}`)
-    socket.emit("chat:leave-room", room)
-  }
+    socket.emit("channel:leave-room", room)
+  })
 
-  const sendChatMessage = (room, senderId, message) => {
-    console.log("in sendChatMessage")
-    sendMessage(room, senderId, message)
-    socket.to(room).emit("chat:send-message", message)
-  }
+  socket.on("channel:send-message", async (room, message, cb) => {
+    const channel = await Channel.findById(room)
+    if (!channel)
+      return socket.emit("channel:send-message", false, "Channel doesnt exist")
 
-  socket.on("chat:join-room", joinRoom)
-  socket.on("chat:leave-room", leaveRoom)
-  socket.on("chat:send-message", sendChatMessage)
+    const [inClass, msg] = await isUserInClass(user._id, channel.classId)
+
+    if (!inClass)
+      return socket.emit("channel:send-message", false, "Not In Class")
+
+    const isUserInRoom = socket.rooms.has(room)
+    if (!isUserInRoom)
+      return socket.emit(
+        "channel:send-message",
+        false,
+        "You are not in the room"
+      )
+
+    if (channel.writePermission === "all") {
+      sendMessage(room, user._id, message)
+      return socket.to(room).emit("channel:send-message", true, message)
+    }
+
+    if (channel.writePermission === "instructor") {
+      if (user.role !== "instructor")
+        return socket.emit("channel:send-message", false, "You Have No Access")
+
+      sendMessage(room, user._id, message)
+      return socket.to(room).emit("channel:send-message", true, message)
+    }
+  })
 }
 
 module.exports = registerChatHandler
