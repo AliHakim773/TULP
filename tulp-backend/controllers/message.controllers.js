8be27200e4b9deff30b4c95405255c0b679ad7e0
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 19:07:22 +0200

    Backend: removed comment

--- tulp-backend/controllers/message.controllers.js
+++ tulp-backend/controllers/message.controllers.js
@@ -1,15 +1,12 @@
 const Channel = require("../models/channel.model")
 
-const sendMessage = async (reciverId, senderId, content) => {
+const sendMessage = async (channelId, senderId, content) => {
   const message = {
     senderId,
     content,
   }
-  // TODO: a lot of missing validation such as checking if the user can send
-  //todo---- msg into the channel by getting all allowed ids from the channel
-  console.log(message)
   try {
-    const channel = await Channel.findById(reciverId)
+    const channel = await Channel.findById(channelId)
     channel.messages.push(message)
     await channel.save()
 
