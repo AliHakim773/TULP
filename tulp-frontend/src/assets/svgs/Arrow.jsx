b7e86c36760146c695c6dd2c30a7ab6d2715c417
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 13:58:58 +0200

    Frontend: added Arrow SVG

---
+++ tulp-frontend/src/assets/svgs/Arrow.jsx
@@ -0,0 +1,18 @@
+const ArrowSVG = ({ className = "" }) => {
+  return (
+    <svg
+      width='27'
+      height='21'
+      viewBox='0 0 27 21'
+      fill='none'
+      className={className}
+      xmlns='http://www.w3.org/2000/svg'>
+      <path
+        d='M24.6569 7.86039L3.46488 0.729005C1.0115 -0.0849018 -0.780368 2.94855 1.03578 4.66443L7.27927 10.5632L1.38046 16.8067C-0.335431 18.6229 1.62529 21.5501 4.02889 20.598L24.7823 12.276C26.6372 11.4496 26.5557 8.57951 24.6569 7.86039Z'
+        fill='black'
+      />
+    </svg>
+  )
+}
+
+export default ArrowSVG
