b5d24807d1c9616012a7c162ad427c4892caf80f
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 16:11:03 +0200

    Backend: added user check in middleware

--- tulp-backend/middlewares/instructor.middleware.js
+++ tulp-backend/middlewares/instructor.middleware.js
@@ -1,5 +1,5 @@
 const instructorMiddleware = async (req, res, next) => {
-  if (req.user.role === "instructor") next()
+  if (req.user && req.user.role === "instructor") next()
   else return res.status(401).send({ message: "Unauthorized" })
 }
 
