51e7717f11592fe3d1a2230623124deb0b037d97
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 16:12:53 +0200

    Frontend: populated channels based on prop

--- tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
+++ tulp-frontend/src/components/Chat/ChatAsideLeft/index.jsx
@@ -1,10 +1,11 @@
-import { useNavigate } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import SettingsDots from "../../Base/SettingsDots"
 
-const ChatAsideLeft = () => {
+const ChatAsideLeft = ({ channels }) => {
   const navigate = useNavigate()
+
   return (
-    <div className='border chat-aside chat-aside-left'>
+    <div className='border chat-aside chat-aside-left flex column'>
       <div
         className='chat-aside-nav chat-aside-item'
         onClick={() => navigate("/class/test-after/stream")}>
@@ -13,13 +14,16 @@ const ChatAsideLeft = () => {
       <div className='chat-aside-header chat-aside-item'>
         Channel <SettingsDots className='chat-aside-settings' />
       </div>
-      <div className='chat-aside-item' onClick={() => navigate("general")}>
-        Some Channel
-      </div>
-      <div className='chat-aside-item' onClick={() => navigate("lol")}>
-        Some Channel
-      </div>
-      <div className='chat-aside-item'>Some Channel</div>
+      {channels.map((channel) => {
+        return (
+          <NavLink
+            key={channel._id}
+            to={channel.name}
+            className='chat-aside-item'>
+            {channel.name}
+          </NavLink>
+        )
+      })}
     </div>
   )
 }
