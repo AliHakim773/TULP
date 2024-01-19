851c78a582332eda8f8987046edf67301b77cfe3
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 13:59:13 +0200

    Frontend: added chat main inputs

--- tulp-frontend/src/components/Chat/ChatMain/index.jsx
+++ tulp-frontend/src/components/Chat/ChatMain/index.jsx
@@ -1,7 +1,27 @@
-import "./styles.css"
+import ArrowSVG from "../../../assets/svgs/Arrow"
 
 const ChatMain = () => {
-  return <div></div>
+  return (
+    <div className='border chat-main flex column'>
+      <div className='chat-main-body flex'>
+        <div className='text'>Hello</div>
+        <div className='text'>Hello</div>
+        <div className='text'>Hello</div>
+      </div>
+      <div className='chat-main-input'>
+        <form className='flex'>
+          <input placeholder={`Talk To me`} type='text' />
+          <button
+            type='submit'
+            onClick={(e) => {
+              e.preventDefault()
+            }}>
+            <ArrowSVG className='send-arrow' />
+          </button>
+        </form>
+      </div>
+    </div>
+  )
 }
 
 export default ChatMain
