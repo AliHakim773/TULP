7b6968bf9a0a8647f07d135df824c001c1fdacd8
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 22:01:57 +0200

    Backend: added timestamps

--- tulp-backend/models/message.model.js
+++ tulp-backend/models/message.model.js
@@ -1,16 +1,21 @@
 const { default: mongoose } = require("mongoose")
 
-const MessageSchema = new mongoose.Schema({
-  senderId: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: "SenderId is required",
+const MessageSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: "SenderId is required",
+    },
+    content: {
+      type: String,
+      required: "Content is required",
+    },
+    files: [String],
   },
-  content: {
-    type: String,
-    required: "Content is required",
-  },
-  files: [String],
-})
+  {
+    timestamps: true,
+  }
+)
 
 module.exports = MessageSchema
