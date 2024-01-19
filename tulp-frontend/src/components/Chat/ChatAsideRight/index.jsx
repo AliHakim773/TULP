e71c7ff217ec33f4f23e9e4a710c5d436672c506
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 16:13:03 +0200

    Frontend: populated poeple based on prop

--- tulp-frontend/src/components/Chat/ChatAsideRight/index.jsx
+++ tulp-frontend/src/components/Chat/ChatAsideRight/index.jsx
@@ -1,10 +1,20 @@
-const ChatAsideRight = () => {
+import { NavLink } from "react-router-dom"
+
+const ChatAsideRight = ({ people }) => {
   return (
-    <div className='border chat-aside chat-aside-right'>
+    <div className='border chat-aside chat-aside-right flex column'>
       <div className='chat-aside-header chat-aside-item'>People</div>
-      <div className='chat-aside-item'>Someone</div>
-      <div className='chat-aside-item'>Someone</div>
-      <div className='chat-aside-item'>Someone</div>
+      {people &&
+        people.map((user) => {
+          return (
+            <NavLink
+              to={user.username}
+              key={user._id}
+              className='chat-aside-item'>
+              {user.username}
+            </NavLink>
+          )
+        })}
     </div>
   )
 }
