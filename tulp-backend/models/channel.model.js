b868a221f98c34536c3692d70847e16776705064
AliHakim773 <AliHakim773@gmail.com>
2024-01-19 19:10:48 +0200

    Backend: removed slug and made name as a slug for channel model

--- tulp-backend/models/channel.model.js
+++ tulp-backend/models/channel.model.js
@@ -5,7 +5,7 @@ const validatePermission = (permission) => {
   return permission_enum.includes(permission)
 }
 
-const permission_enum = ["instructor", "student", "all"]
+const permission_enum = ["instructor", "all"]
 
 const ChannelSchema = new mongoose.Schema({
   classId: {
@@ -18,29 +18,23 @@ const ChannelSchema = new mongoose.Schema({
     minlength: [3, "Name length must be more than 3"],
     maxlength: [40, "Name length must be less than 40"],
   },
-  slug: {
-    type: String,
-    unique: true,
-  },
   readPermission: {
     type: String,
     enum: permission_enum,
     validate: [validatePermission, "{VALUE} permission doesnt exist"],
     default: "all",
   },
-  // TODO: array of read permissions
   writePermission: {
     type: String,
     enum: permission_enum,
     validate: [validatePermission, "{VALUE} permission doesnt exist"],
     default: "all",
   },
-  // TODO: array of write permissions
   messages: [MessageSchema],
 })
 
 ChannelSchema.pre("save", function (next) {
-  this.slug = this.name.trim().split(" ").join("-").toLowerCase()
+  this.name = this.name.trim().split(" ").join("-").toLowerCase()
   next()
 })
 
