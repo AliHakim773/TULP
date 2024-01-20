edf8a6e21b202619bfd789624b64bf29a554ea72
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 17:14:06 +0200

    Frontend: initialized VideoClass Page

--- tulp-frontend/src/pages/VideoClass/index.jsx
+++ tulp-frontend/src/pages/VideoClass/index.jsx
@@ -1,9 +1,27 @@
+import HomeScreen from "../../components/VideoRoom/HomeScreen"
 import "./styles.css"
 import useVideoClassLogic from "./useVideoClassLogic"
+import { DailyProvider } from "@daily-co/daily-react"
 
 const VideoClass = () => {
-  const {} = useVideoClassLogic()
-  return <div></div>
+  const {
+    showCall,
+    showHairCheck,
+    callObject,
+    apiError,
+    createCall,
+    startHairCheck,
+    startLeavingCall,
+  } = useVideoClassLogic()
+
+  const renderApp = () => {
+    // The default view is the HomeScreen, from where we start the demo.
+    return (
+      <HomeScreen createCall={createCall} startHairCheck={startHairCheck} />
+    )
+  }
+
+  return <div className='app'>{renderApp()}</div>
 }
 
 export default VideoClass
