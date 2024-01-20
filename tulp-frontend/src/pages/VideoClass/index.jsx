133ee4aa02029322878350ebc936ae492d166faf
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 17:47:54 +0200

    Frontend: added dailyco api error handling

--- tulp-frontend/src/pages/VideoClass/index.jsx
+++ tulp-frontend/src/pages/VideoClass/index.jsx
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom"
+import Button from "../../components/Base/Button"
+import NavBar from "../../components/UI/NavBar"
 import HomeScreen from "../../components/VideoRoom/HomeScreen"
 import "./styles.css"
 import useVideoClassLogic from "./useVideoClassLogic"
 import { DailyProvider } from "@daily-co/daily-react"
 
 const VideoClass = () => {
+  const navigate = useNavigate()
+
   const {
     showCall,
     showHairCheck,
@@ -15,6 +20,31 @@ const VideoClass = () => {
   } = useVideoClassLogic()
 
   const renderApp = () => {
+    if (apiError) {
+      return (
+        <div className='page'>
+          <NavBar />
+          <div className='api-error flex column center'>
+            <h1>Error</h1>
+            <p>
+              Room could not be created. Check if your `.env` file is set up
+              correctly. For more information, see the{" "}
+              <a href='https://github.com/daily-demos/custom-video-daily-react-hooks#readme'>
+                readme
+              </a>{" "}
+              :)
+            </p>
+            <div className=''>
+              <Button
+                text='Go Back To Class Room'
+                onclick={() => navigate(-1)}
+              />
+            </div>
+          </div>
+        </div>
+      )
+    }
+
     // The default view is the HomeScreen, from where we start the demo.
     return (
       <HomeScreen createCall={createCall} startHairCheck={startHairCheck} />
