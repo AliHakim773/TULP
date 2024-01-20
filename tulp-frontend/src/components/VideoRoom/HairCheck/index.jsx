fee9a2747eb07fe605e5fe8bb5728b5bc03e6afe
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 18:04:57 +0200

    Frontend: added mic and camera check

--- tulp-frontend/src/components/VideoRoom/HairCheck/index.jsx
+++ tulp-frontend/src/components/VideoRoom/HairCheck/index.jsx
@@ -1,5 +1,22 @@
-const HairCheck = () => {
-  return <div></div>
+import UserMediaError from "../UserMediaError"
+import useHairCheckLogic from "./useHairCheckLogic"
+
+const HairCheck = ({ joinCall, cancelCall }) => {
+  const {
+    updateCamera,
+    updateSpeakers,
+    updateMicrophone,
+    handleJoin,
+    handleChange,
+    getUserMediaError,
+    speakers,
+    cameras,
+    microphones,
+    currentSpeaker,
+    currentMic,
+    currentCam,
+  } = useHairCheckLogic(joinCall)
+  return getUserMediaError ? <UserMediaError /> : <form></form>
 }
 
 export default HairCheck
