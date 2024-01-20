9e9a101ede7e53ce15c8edd070ad18ccef637dea
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 12:49:05 +0200

    Backend: called registerDMHandler function in socket index

--- tulp-backend/sockets/index.js
+++ tulp-backend/sockets/index.js
@@ -2,15 +2,17 @@ const io = require("./index.js")
 const registerChatHandler = require("./registerChatHandler")
 const jwt = require("jsonwebtoken")
 const User = require("../models/user.model")
+const registerDMHandler = require("./registerDMHandler.js")
 
 const onConnection = async (socket) => {
-  const bearerToken = socket.handshake.query.token
+  const bearerToken = socket.handshake.headers.token
   if (bearerToken) {
     const token = bearerToken.split(" ")[1]
     try {
       decode = jwt.verify(token, process.env.JWT_SECRET)
       const user = await User.findOne({ _id: decode._id }).select("-password")
       registerChatHandler(io, socket, user)
+      registerDMHandler(io, socket, user)
     } catch (e) {
       return
     }
