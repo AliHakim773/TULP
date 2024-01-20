5b9aaeb5a064341ef69606a17d96fcc708b35531
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 15:23:20 +0200

    Frontend: added Chat Dms

--- tulp-frontend/src/components/Chat/ChatDmMain/index.jsx
+++ tulp-frontend/src/components/Chat/ChatDmMain/index.jsx
@@ -1,29 +1,25 @@
 import { useState } from "react"
 import ArrowSVG from "../../../assets/svgs/Arrow"
 import ChatMessage from "../ChatMessage"
+import useChatDM from "./useChatDM"
 
 const ChatDmMain = () => {
-  const [inputValue, setInputValue] = useState("")
-  const someUser = {
-    username: "ali",
-    imageUrl: "uploads/default.png",
-    _id: "sdgf",
-    time: "Today",
-  }
 
-  const handleKeyDown = (e) => {
-    if (e.ctrlKey && e.key === "Enter") {
-      e.preventDefault()
-    }
-  }
+  const {
+    handleSendMessage,
+    handleKeyDown,
+    handleOnInputChange,
+    inputValue,
+    messages,
+    disabled,
+  } = useChatDM()
 
   return (
     <div className='border chat-main flex column'>
       <div className='chat-main-body flex'>
-        <ChatMessage text={"Hello"} user={someUser} />
-        <ChatMessage text={"Hello"} user={someUser} />
-        <ChatMessage text={"Hello"} user={someUser} />
-        <ChatMessage text={"Hello"} user={someUser} />
+        {messages.toReversed().map((m) => {
+          return <ChatMessage key={m._id} text={m.content} user={m.senderId} />
+        })}
       </div>
       <div className='chat-main-input'>
         <form className='flex'>
@@ -31,13 +27,14 @@ const ChatDmMain = () => {
             placeholder={`Message general`}
             type='text'
             value={inputValue}
-            onChange={(e) => setInputValue(e.target.value)}
+            onChange={handleOnInputChange}
             onKeyDown={handleKeyDown}
           />
           <button
             type='submit'
             onClick={(e) => {
               e.preventDefault()
+              handleSendMessage()
             }}>
             <ArrowSVG className='send-arrow' />
           </button>
---
+++ tulp-frontend/src/components/Chat/ChatDmMain/useChatDM.js
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react"
+import { useSelector } from "react-redux"
+import { extractUserSlice } from "../../../core/redux/userSlice"
+import { useLoaderData, useLocation, useParams } from "react-router-dom"
+import { channelAPI } from "../../../core/api/channel"
+import { socket } from "../../../core/socket"
+
+const useChatDM = () => {
+  const data = useLoaderData()
+  const pathname = useLocation()
+  const userSlice = useSelector(extractUserSlice)
+  const { slug, username } = useParams()
+
+  const [inputValue, setInputValue] = useState("")
+  const [messages, setMessages] = useState(data.dms.messages)
+
+  const sendMessageListener = async (body) => {
+    try {
+      setMessages((prev) => [
+        ...prev,
+        {
+          _id: new Date(),
+          senderId: {
+            ...data.user,
+          },
+          content: body,
+        },
+      ])
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await channelAPI.getDMs(slug, username)
+        setMessages(res.dms.messages)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getMessages()
+
+    socket.connect()
+    socket.emit("dm:join-room", data.dms.classId, data.user._id, (mes) => {
+      console.log(mes)
+    })
+    if (!socket.hasListeners("dm:send-message")) {
+      socket.on("dm:send-message", sendMessageListener)
+    }
+
+    return () => {
+      socket.removeListener("dm:send-message", sendMessageListener)
+      socket.emit("dm:leave-room", data.dms.classId, data.user._id, (msg) => {
+        console.log(msg)
+      })
+      socket.disconnect()
+    }
+  }, [pathname])
+
+  const handleSendMessage = async () => {
+    socket.emit(
+      "dm:send-message",
+      data.dms._id,
+      data.user._id,
+      inputValue.replace(/\n+$/, ""),
+      (msg) => {
+        console.log(msg)
+      }
+    )
+    setMessages((prev) => [
+      ...prev,
+      {
+        _id: new Date(),
+        senderId: {
+          username: userSlice.username,
+          imageUrl: userSlice.imageUrl,
+        },
+        content: inputValue.replace(/\n+$/, ""),
+      },
+    ])
+    setInputValue("")
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.ctrlKey && e.key === "Enter") {
+      e.preventDefault()
+      handleSendMessage()
+    }
+  }
+
+  const handleOnInputChange = (e) => setInputValue(e.target.value)
+
+  return {
+    handleSendMessage,
+    handleOnInputChange,
+    handleKeyDown,
+    inputValue,
+    messages,
+  }
+}
+
+export default useChatDM
