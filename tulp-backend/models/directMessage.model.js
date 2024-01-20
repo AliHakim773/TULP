a6bf8a1db26e1dbb671594d16dcb80c1271845e3
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 12:48:09 +0200

    Backend: added DirectMessage Model

--- tulp-backend/models/directMessage.model.js
+++ tulp-backend/models/directMessage.model.js
@@ -1,18 +1,18 @@
 const { default: mongoose } = require("mongoose")
+const MessageSchema = require("./message.model")
 
 const DirectMessageSchema = new mongoose.Schema({
+  classId: {
+    type: mongoose.Types.ObjectId,
+    ref: "Class",
+  },
   edges: [
     {
       type: mongoose.Types.ObjectId,
-      ref: "user",
+      ref: "User",
     },
   ],
   messages: [MessageSchema],
-})
-
-ChannelSchema.pre("save", function (next) {
-  this.name = this.name.trim().split(" ").join("-").toLowerCase()
-  next()
 })
 
 const DirectMessage = mongoose.model("DirectMessage", DirectMessageSchema)
