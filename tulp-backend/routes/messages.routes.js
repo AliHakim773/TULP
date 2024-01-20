ec0441cf06e68d49e6cb40607fac8b0b68fa2221
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 13:51:43 +0200

    Backend: added getDMMessages route

--- tulp-backend/routes/messages.routes.js
+++ tulp-backend/routes/messages.routes.js
@@ -2,9 +2,11 @@ const express = require("express")
 const {
   getChannelMessages,
   getUserByIdForChat,
+  getDMMessages,
 } = require("../controllers/message.controllers")
 const router = express.Router()
 
+router.get("/dm/:slug/:username", getDMMessages)
 router.get("/:slug/:name", getChannelMessages)
 router.get("/:id", getUserByIdForChat)
 
