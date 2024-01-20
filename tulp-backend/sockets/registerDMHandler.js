5250532c54fce9ccc5bc8f56a6915d2fadf78a19
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 12:59:13 +0200

    Backend: added dm:leave-room event

--- tulp-backend/sockets/registerDMHandler.js
+++ tulp-backend/sockets/registerDMHandler.js
@@ -3,20 +3,46 @@ const DirectMessage = require("../models/directMessage.model")
 
 const registerDMHandler = (io, socket, user) => {
   socket.on("dm:join-room", async (classId, userId, cb) => {
-    const [user1, msg1] = await isUserInClass(user._id, classId)
-    if (!user1) return cb("User 1 is not in class")
-    console.log(await isUserInClass(userId, classId))
-    const [user2, msg2] = await isUserInClass(userId, classId)
-    if (!user2) return cb("User 2 is not in class")
+    try {
+      const [user1, msg1] = await isUserInClass(user._id, classId)
+      if (!user1) return cb("User 1 is not in class")
+      const [user2, msg2] = await isUserInClass(userId, classId)
+      if (!user2) return cb("User 2 is not in class")
 
-    let dm = await DirectMessage.findOne({
-      edges: { $all: [user._id, userId] },
-    })
-    if (!dm) dm = await DirectMessage.create({ edges: [user._id, userId] })
+      let dm = await DirectMessage.findOne({
+        classId: classId,
+        edges: { $all: [user._id, userId] },
+      })
+      if (!dm)
+        dm = await DirectMessage.create({
+          classId: classId,
+          edges: [user._id, userId],
+        })
 
-    socket.join(dm._id)
+      socket.join(dm._id)
 
-    cb(`Joined Room ${dm._id}`)
+      cb(`Joined Room ${dm._id}`)
+    } catch (e) {
+      cb(e)
+    }
+  })
+
+  socket.on("dm:leave-room", async (classId, userId, cb) => {
+    try {
+      let dm = await DirectMessage.findOne({
+        classId: classId,
+        edges: { $all: [user._id, userId] },
+      })
+
+      if (dm) {
+        socket.leave(dm._id)
+        return cb("Left Room")
+      }
+
+      cb("Room doesnt exist")
+    } catch (e) {
+      cb(e)
+    }
   })
 }
 
