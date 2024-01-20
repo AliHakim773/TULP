d3816f9e35da8cd6abb4ab983101ade25db33f05
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 16:49:26 +0200

    Frontend: added page state

--- tulp-frontend/src/pages/VideoClass/useVideoClassLogic.jsx
+++ tulp-frontend/src/pages/VideoClass/useVideoClassLogic.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+
+// page state
+const STATE_IDLE = "STATE_IDLE"
+const STATE_CREATING = "STATE_CREATING"
+const STATE_JOINING = "STATE_JOINING"
+const STATE_JOINED = "STATE_JOINED"
+const STATE_LEAVING = "STATE_LEAVING"
+const STATE_ERROR = "STATE_ERROR"
+const STATE_HAIRCHECK = "STATE_HAIRCHECK"
+
 const useVideoClassLogic = () => {
+  const [appState, setAppState] = useState(STATE_IDLE)
   return {}
 }
 
