7b57569b78beb5fc8f8bc166f1a3ad774aac3aba
AliHakim773 <AliHakim773@gmail.com>
2024-01-20 17:39:52 +0200

    Frontend: added homescreen component

--- tulp-frontend/src/components/VideoRoom/HomeScreen/index.jsx
+++ tulp-frontend/src/components/VideoRoom/HomeScreen/index.jsx
@@ -1,5 +1,23 @@
+import Button from "../../Base/Button"
+import NavBar from "../../UI/NavBar"
+
 const HomeScreen = ({ createCall, startHairCheck }) => {
-  return <div></div>
+  const startDemo = () => {
+    createCall().then((url) => {
+      startHairCheck(url)
+    })
+  }
+
+  return (
+    <div className='page'>
+      <NavBar />
+      <div className='home-screen flex column center'>
+        <div className='join-btn'>
+          <Button onclick={startDemo} text='Click to start the Class' />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default HomeScreen
