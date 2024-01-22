feed4a55f351a76048cd368d566d2d48421dc139
AliHakim773 <AliHakim773@gmail.com>
2024-01-22 11:27:44 +0200

    Frontemd: added tile component

---
+++ tulp-frontend/src/components/VideoRoom/Tile/index.jsx
@@ -0,0 +1,37 @@
+import { DailyVideo, useVideoTrack } from "@daily-co/daily-react"
+import "./styles.css"
+import Username from "../Username"
+
+const Tile = ({ id, isScreenShare, isLocal, isAlone, className, onClick }) => {
+  const videoState = useVideoTrack(id)
+
+  let containerCssClasses = isScreenShare ? "tile-screenshare" : "tile-video"
+
+  if (isLocal) {
+    containerCssClasses += " self-view"
+    if (isAlone) {
+      containerCssClasses += " alone"
+    }
+  }
+
+  /* If a participant's video is muted, hide their video and
+  add a different background color to their tile. */
+  if (videoState.isOff) {
+    containerCssClasses += " no-video"
+  }
+
+  return (
+    <div
+      onClick={onClick}
+      className={`video-item ${containerCssClasses} ${className}`}>
+      <DailyVideo
+        className={className}
+        sessionId={id}
+        type={isScreenShare ? "screenVideo" : "video"}
+      />
+      {!isScreenShare && <Username id={id} isLocal={isLocal} />}
+    </div>
+  )
+}
+
+export default Tile
