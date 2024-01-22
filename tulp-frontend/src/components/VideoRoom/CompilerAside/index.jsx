8d5c1fcaed469fe1e6bee887bdee3987cecf9eeb
AliHakim773 <AliHakim773@gmail.com>
2024-01-22 16:49:24 +0200

    Frontend: initialized CompilerAside

---
+++ tulp-frontend/src/components/VideoRoom/CompilerAside/index.jsx
@@ -0,0 +1,21 @@
+import "./styles.css"
+import {
+  useLocalSessionId,
+  useParticipantProperty,
+} from "@daily-co/daily-react"
+
+const CompilerAside = ({ showCode, toggleCode }) => {
+  const localSessionId = useLocalSessionId()
+  const username = useParticipantProperty(localSessionId, "user_name")
+
+  return showCode ? (
+    <aside className='video-chat flex column shadow'>
+      <button onClick={toggleCode} className='close-chat' type='button'>
+        Close
+      </button>
+      <ul className='video-chat-messages '></ul>
+    </aside>
+  ) : null
+}
+
+export default CompilerAside
