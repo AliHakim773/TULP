ac00ff87bb314bd5d29bbb7e9a21e1f8c69ca291
AliHakim773 <AliHakim773@gmail.com>
2024-01-22 19:35:08 +0200

    Frontend: added create room and emited the event

--- tulp-frontend/src/components/VideoRoom/HomeScreen/index.jsx
+++ tulp-frontend/src/components/VideoRoom/HomeScreen/index.jsx
@@ -1,10 +1,21 @@
+import { useLocation, useParams } from "react-router-dom"
 import Button from "../../Base/Button"
 import NavBar from "../../UI/NavBar"
+import { socket } from "../../../core/socket"
+import toast from "react-hot-toast"
+import { roomAPI } from "../../../core/api/room"
 
 const HomeScreen = ({ createCall, startHairCheck }) => {
+  const { pathname } = useLocation()
+  const { slug } = useParams()
   const startDemo = () => {
-    createCall().then((url) => {
+    createCall().then(async (url) => {
       startHairCheck(url)
+      const roomUrl = pathname + "?roomUrl=" + url
+      try {
+        await roomAPI.createRoom({ slug, url: roomUrl })
+        socket.emit("room:create", slug, (msg) => {})
+      } catch (e) {}
     })
   }
 
