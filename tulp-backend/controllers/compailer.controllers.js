3320020f79635bf79e12de350338b474325a98cc
AliHakim773 <AliHakim773@gmail.com>
2024-01-22 18:17:37 +0200

    Backend: added room api functions in compiler controllers

--- tulp-backend/controllers/compailer.controllers.js
+++ tulp-backend/controllers/compailer.controllers.js
@@ -1,4 +1,5 @@
 const sendRequest = require("../helpers/axios")
+const Class = require("../models/class.model")
 
 const compileCode = async (req, res) => {
   const { content, lang } = req.body
@@ -40,4 +41,62 @@ const compileCode = async (req, res) => {
   return res.send("error")
 }
 
-module.exports = { compileCode }
+const createRoom = async (req, res) => {
+  const { slug, url } = req.body
+  try {
+    const classObject = await Class.findOne({ slug })
+    classObject.room = url
+    await classObject.save()
+
+    res.status(200).send({ url })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+const deleteRoom = async (req, res) => {
+  const { slug } = req.body
+  try {
+    const classObject = await Class.findOne({ slug })
+    classObject.room = ""
+    await classObject.save()
+
+    res.status(200).send({ message: "Room removed" })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+const addParticipant = async (req, res) => {
+  const { slug, username } = req.body
+  try {
+    const classObject = await Class.findOne({ slug })
+    classObject.participants.push(username)
+    await classObject.save()
+
+    res.status(200).send({ participants: classObject.participants })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+const removeParticipant = async (req, res) => {
+  const { slug, username } = req.body
+  try {
+    const classObject = await Class.findOne({ slug })
+    classObject.participants = classObject.participants.filter(
+      (name) => name !== username
+    )
+
+    await classObject.save()
+
+    res.status(200).send({ participants: classObject.participants })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+
+module.exports = {
+  compileCode,
+  createRoom,
+  deleteRoom,
+  addParticipant,
+  removeParticipant,
+}
--- tulp-backend/routes/compile.routes.js
+++ tulp-backend/routes/compile.routes.js
@@ -1,7 +1,17 @@
 const express = require("express")
-const { compileCode } = require("../controllers/compailer.controllers")
+const {
+  compileCode,
+  createRoom,
+  deleteRoom,
+  addParticipant,
+  removeParticipant,
+} = require("../controllers/compailer.controllers")
 const router = express.Router()
 
 router.post("/", compileCode)
+router.post("/create-room", createRoom)
+router.post("/delete-room", deleteRoom)
+router.post("/add-participant", addParticipant)
+router.post("/remove-participant", removeParticipant)
 
 module.exports = router
