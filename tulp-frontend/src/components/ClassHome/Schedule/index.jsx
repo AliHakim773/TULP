068ec9962ae0fdb3773d247977982d811b466990
AliHakim773 <AliHakim773@gmail.com>
2024-01-22 18:44:46 +0200

    Frontend: added listners to class create

--- tulp-frontend/src/components/ClassHome/Schedule/index.jsx
+++ tulp-frontend/src/components/ClassHome/Schedule/index.jsx
@@ -4,12 +4,30 @@ import ScheduleItem from "./ScheduleItem"
 import "./styles.css"
 import { useSelector } from "react-redux"
 import { extractUserSlice } from "../../../core/redux/userSlice"
+import { useEffect, useState } from "react"
+import { socket } from "../../../core/socket"
 
 const Schedule = () => {
   const navigate = useNavigate()
   const data = useLoaderData()
   const { slug } = useParams()
   const { role } = useSelector(extractUserSlice)
+  const [url, setUrl] = useState(data.roomUrl)
+
+  const roomCreateListner = (room) => {
+    console.log(room)
+    setUrl(room)
+  }
+
+  useEffect(() => {
+    if (!socket.hasListeners("room:create")) {
+      socket.on("room:create", roomCreateListner)
+    }
+    console.log("hello")
+    return () => {
+      socket.removeListener("room:create", roomCreateListner)
+    }
+  }, [])
 
   return (
     <>
@@ -17,23 +35,33 @@ const Schedule = () => {
       <div className='schedule'>
         <div className='schedule-header flex'>
           {role !== "student" && (
+            <>
+              <Button
+                text='Add'
+                onclick={() => {
+                  navigate(`add`)
+                }}
+              />
+              <Button
+                text='Create'
+                onclick={() => {
+                  navigate(`/class/${slug}/room`)
+                }}
+              />
+            </>
+          )}
+          {url && (
             <Button
-              text='Add'
+              text='Join'
               onclick={() => {
-                navigate(`add`)
+                navigate(url)
               }}
             />
           )}
-          <Button
-            text='Join'
-            onclick={() => {
-              navigate(`/class/${slug}/room`)
-            }}
-          />
         </div>
 
-        {data &&
-          data.map((schedule, i) => {
+        {data.schedule &&
+          data.schedule.map((schedule, i) => {
             return <ScheduleItem key={i} schedule={schedule} />
           })}
       </div>
