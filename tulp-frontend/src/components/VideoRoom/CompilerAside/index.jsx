69a273ec5d402fb7c3edc6c94f05f6e69e25f82a
AliHakim773 <AliHakim773@gmail.com>
2024-01-23 15:43:27 +0200

    Frontend: removed participants

--- tulp-frontend/src/components/VideoRoom/CompilerAside/index.jsx
+++ tulp-frontend/src/components/VideoRoom/CompilerAside/index.jsx
@@ -1,39 +1,15 @@
 import { useParams } from "react-router-dom"
 import { socket } from "../../../core/socket"
 import "./styles.css"
-import {
-  useLocalSessionId,
-  useParticipantProperty,
-} from "@daily-co/daily-react"
-import { useEffect, useState } from "react"
 
 const CompilerAside = ({ showCode, toggleCode, setShowCompiler }) => {
   const { slug } = useParams()
-  const [participants, setParticipants] = useState([])
-  const localSessionId = useLocalSessionId()
   const handleShowCompiler = () => {
     setShowCompiler((prev) => {
       socket.emit("room:toggle-compiler", slug, !prev)
       return !prev
     })
   }
-
-  const handleParticipantToggle = (e) => {
-    socket.emit("room:toggle-participants", slug, e.target.checked)
-  }
-
-  useEffect(() => {
-    if (!socket.hasListeners("room:update-participants")) {
-      socket.on("room:update-participants", (value) => {
-        setParticipants(value)
-      })
-    }
-    return () => {
-      socket.removeListener("room:update-participants", (value) => {
-        setParticipants(value)
-      })
-    }
-  }, [])
 
   return showCode ? (
     <aside className='video-chat flex column shadow'>
@@ -48,21 +24,6 @@ const CompilerAside = ({ showCode, toggleCode, setShowCompiler }) => {
             <span class='slider'></span>
           </label>
         </li>
-        {/* <li>
-          <h4>Permissions</h4>
-        </li>
-        {participants &&
-          participants.map((p) => {
-            return (
-              <li key={p}>
-                <span>{p}</span>
-                <label class='switch'>
-                  <input type='checkbox' onChange={handleParticipantToggle} />
-                  <span class='slider'></span>
-                </label>
-              </li>
-            )
-          })} */}
       </ul>
     </aside>
   ) : null
