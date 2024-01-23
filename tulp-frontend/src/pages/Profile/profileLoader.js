2246ca12360e1b74e34f99bf4326f743356a9ce1
AliHakim773 <AliHakim773@gmail.com>
2024-01-23 16:32:58 +0200

    Frontend: added userProfile loader

--- tulp-frontend/src/pages/Profile/profileLoader.js
+++ tulp-frontend/src/pages/Profile/profileLoader.js
@@ -1,8 +1,9 @@
 import classAPI from "../../core/api/class"
 
 export const profileLoader = async ({ params }) => {
-  if (params.slug) {
-    return null
+  if (params.username) {
+    const res = await classAPI.getUserProfile(params.username)
+    return res
   } else {
     const res = await classAPI.getClassesIn()
     return res
