5d257f3e798ba6f1db3716d0a443ca044e6208be
AliHakim773 <AliHakim773@gmail.com>
2024-01-24 15:37:29 +0200

    Admin: added login Page

--- admin-panel/src/renderer/src/pages/LoginPage/index.jsx
+++ admin-panel/src/renderer/src/pages/LoginPage/index.jsx
@@ -1,14 +1,65 @@
 import './styles.css'
 import logo from '../../assets/images/icon.png'
+import { Button, IconButton, InputAdornment, TextField } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+import useLoginLogic from './useLoginLogic'
 
 const LoginPage = () => {
+  const {
+    showPassword,
+    error,
+    handleClickShowPassword,
+    handleOnChange,
+    handleOnClick,
+    handleMouseDownPassword
+  } = useLoginLogic()
+
   return (
     <div className="page flex center">
       <div className="login-page flex center">
-        <div className="logo-img">
+        <div className="logo-img ">
           <img src={logo} alt="TULP" />
         </div>
-        <form className="login-form border rounded-1 p-2">Login Form</form>
+        <form className="login-form flex column gap-2 shadow rounded-2 p-2">
+          <h3 className="text-center">Log In</h3>
+          <TextField
+            id="outlined-basic"
+            name="username"
+            required
+            label="Username"
+            variant="outlined"
+            onChange={handleOnChange}
+            error={error.state}
+          />
+          <TextField
+            name="password"
+            id="outlined-basic"
+            error={error.state}
+            helperText={error.message}
+            required
+            type={showPassword ? 'text' : 'password'}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              )
+            }}
+            label="Password"
+            variant="outlined"
+            onChange={handleOnChange}
+          />
+          <Button onClick={handleOnClick} variant="contained">
+            Login
+          </Button>
+        </form>
       </div>
     </div>
   )
