478d24df14cd3c1e8c1afb8fd896620ff956218d
AliHakim773 <AliHakim773@gmail.com>
2024-01-24 15:34:00 +0200

    Backend: added admin login

---
+++ tulp-backend/controllers/auth.admin.controller.js
@@ -0,0 +1,40 @@
+const User = require("../models/user.model")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+
+const adminLogin = async (req, res) => {
+  const { username, password } = req.body
+  try {
+    console.log({ username, password })
+    const user = await User.findOne({ username, role: "admin" })
+    if (!user)
+      return res.status(400).send({ message: "Wrong username or password" })
+
+    const isValidPassword = await bcrypt.compare(password, user.password)
+    if (!isValidPassword)
+      return res.status(400).send({ message: "Wrong username or password" })
+
+    const {
+      password: hashedPassword,
+      socialMediaLinks,
+      enrolledClasses,
+      classes,
+      education,
+      ...userDetails
+    } = user.toJSON()
+
+    const token = jwt.sign(
+      {
+        ...userDetails,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "30 days" }
+    )
+
+    res.status(200).send({ user: userDetails, token })
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+}
+
+module.exports = { adminLogin }
