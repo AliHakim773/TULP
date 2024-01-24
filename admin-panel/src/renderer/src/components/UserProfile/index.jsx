3bbf392820a8861e865c5b9e75c92e7f0282913c
AliHakim773 <AliHakim773@gmail.com>
2024-01-24 20:00:21 +0200

    Admin: added userProfile

--- admin-panel/src/renderer/src/components/UserProfile/index.jsx
+++ admin-panel/src/renderer/src/components/UserProfile/index.jsx
@@ -1,8 +1,21 @@
 import './styles.css'
 
 const UserProfile = ({ user }) => {
-  const { username, email, imageUrl, firstName } = user
-  return <div className="user-profile">d</div>
+  const { username, email, imageUrl, firstName, lastName } = user
+  return (
+    <div className="user-profile flex">
+      <div className="pfp">
+        <img src={`http://localhost:8000/${imageUrl}`} alt="" />
+      </div>
+      <div className="user-info flex column align-start justify-center">
+        <div className="capitalize">Username: {username}</div>
+        <div className="capitalize">
+          Name: {firstName} {lastName}
+        </div>
+        <div>Email: {email}</div>
+      </div>
+    </div>
+  )
 }
 
 export default UserProfile
