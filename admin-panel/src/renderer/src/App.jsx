ac0ff4bacfa978f530025990e6c260c80e142493
AliHakim773 <AliHakim773@gmail.com>
2024-01-24 19:38:32 +0200

    Frontend: added routes for unknown urls and errors

--- admin-panel/src/renderer/src/App.jsx
+++ admin-panel/src/renderer/src/App.jsx
@@ -2,12 +2,15 @@
 import './styles/index.css'
 
 // react router
-import { RouterProvider } from 'react-router-dom'
+import { Navigate, RouterProvider } from 'react-router-dom'
 import { Route, createHashRouter, createRoutesFromElements } from 'react-router-dom'
 
 // auth
 import LoginPage from './pages/LoginPage'
 import AuthLayout from './components/Layouts/AuthLayout'
+
+// layouts
+import NavLayout from './components/Layouts/NavLayout'
 
 // pages
 import HomePage from './pages/HomePage'
@@ -16,9 +19,12 @@ const router = createHashRouter(
   createRoutesFromElements(
     <Route path="/">
       <Route index element={<LoginPage />} />
-      <Route element={<AuthLayout />}>
-        <Route path="home" element={<HomePage />}></Route>
+      <Route element={<AuthLayout />} errorElement={<Navigate to={'/home'} />}>
+        <Route element={<NavLayout />}>
+          <Route path="home" element={<HomePage />}></Route>
+        </Route>
       </Route>
+      <Route path="/*" element={<Navigate to={'/home'} />} />
     </Route>
   )
 )
