ccaf00e2e96a500d8f1a9e5e387ba1abe2da8be8
AliHakim773 <AliHakim773@gmail.com>
2024-01-27 15:26:35 +0200

    Admin: added stats section with number of users

--- admin-panel/src/renderer/src/pages/HomePage/index.jsx
+++ admin-panel/src/renderer/src/pages/HomePage/index.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from 'react'
+import StatItem from '../../components/StatItem'
 import UserProfile from '../../components/UserProfile'
 import { local } from '../../core/helpers/localStorage'
 import './styles.css'
+import statsAPI from '../../core/api/stats'
 
 const HomePage = () => {
   const user = local('user')
+
+  const [userCount, setUserCount] = useState(0)
+  const [userCountLoading, setUserCountLoading] = useState(true)
+
+  useEffect(() => {
+    const getUserCount = async () => {
+      try {
+        const res = await statsAPI.getUsersCount()
+        setUserCount(res.numberOfUsers)
+        setUserCountLoading(false)
+      } catch {}
+    }
+    getUserCount()
+  })
   return (
     <div className="container">
       <UserProfile user={JSON.parse(user)} />
+      <div className="stat-container p-2 flex wrap gap-3">
+        {userCountLoading ? (
+          <StatItem text="Loading" number={0} />
+        ) : (
+          <StatItem text="Users Count" number={userCount} />
+        )}
+      </div>
     </div>
   )
 }
