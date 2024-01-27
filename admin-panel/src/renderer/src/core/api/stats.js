a5ae70572021ddfd9ba007c672a14e93f1e27b42
AliHakim773 <AliHakim773@gmail.com>
2024-01-27 15:29:36 +0200

    Admin: added get students and get instructors count API calls

--- admin-panel/src/renderer/src/core/api/stats.js
+++ admin-panel/src/renderer/src/core/api/stats.js
@@ -6,6 +6,18 @@ const statsAPI = {
       route: 'admin/user/count'
     })
     return res
+  },
+  getStudentsCount: async () => {
+    const res = sendRequest({
+      route: 'admin/students/count'
+    })
+    return res
+  },
+  getInstructorsCount: async () => {
+    const res = sendRequest({
+      route: 'admin/instructors/count'
+    })
+    return res
   }
 }
 
