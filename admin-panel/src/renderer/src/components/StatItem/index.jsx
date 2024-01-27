d890f08a9082430a68224a853b6f83d76cf916e0
AliHakim773 <AliHakim773@gmail.com>
2024-01-27 15:25:49 +0200

    Admin: added stat item component

---
+++ admin-panel/src/renderer/src/components/StatItem/index.jsx
@@ -0,0 +1,19 @@
+import './styles.css'
+
+const StatItem = ({ text = 'Default Text', number = 400, percentage, percentageOf }) => {
+  return (
+    <div className="stat-item border flex column center gap-1">
+      <h3>{text}</h3>
+      <div className="stat-number w-100 text-center">
+        {number}{' '}
+        {percentage ? (
+          <span>
+            {percentage}% of {percentageOf}
+          </span>
+        ) : null}
+      </div>
+    </div>
+  )
+}
+
+export default StatItem
